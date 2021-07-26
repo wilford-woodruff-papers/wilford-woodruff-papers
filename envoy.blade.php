7f1929610d63e4a08d1b5ba9792f65ba9287e643
Jon Fackrell <jon.fackrell@gmail.com>
2021-07-26 08:02:42 -0600

    Added envoyer deployment

--- envoy.blade.php
+++ envoy.blade.php
@@ -8,8 +8,16 @@
 @endtask
 
 @task('dev', ['on' => 'web'])
-    cd /home/wilfordw/dev.wilfordwoodruffpapers.org/production
+    cd /home/wilfordw/dev.wilfordwoodruffpapers.org/develop
+    git pull origin develop --ff-only
+    composer install
+    php artisan migrate --force
+@endtask
+
+@task('prod', ['on' => 'web'])
+    cd /home/wilfordw/production_laravel_app
     git pull origin main --ff-only
+    
     composer install
     php artisan migrate --force
 @endtask
