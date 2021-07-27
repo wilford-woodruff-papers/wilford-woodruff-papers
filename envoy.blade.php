7ca77e98064f0a255f7311ca771e4cb615f2fa21
Jon Fackrell <jon.fackrell@gmail.com>
2021-07-27 11:39:02 -0600

    Develop (#5)
    * Removed dupilcate subjects.show route name and unused routes to enable route caching
    
    * Changed LFM config to prevent duplicate routes
    
    * Only show people who have pages

--- envoy.blade.php
+++ envoy.blade.php
@@ -12,12 +12,22 @@
     git pull origin develop --ff-only
     composer install
     php artisan migrate --force
+    php artisan route:cache
 @endtask
 
 @task('prod', ['on' => 'web'])
     cd /home/wilfordw/production_laravel_app
     git pull origin main --ff-only
 
+    cp -R /home/wilfordw/production_laravel_app/public/css/* /home/wilfordw/public_html/css
+    cp -R /home/wilfordw/production_laravel_app/public/js/* /home/wilfordw/public_html/js
+    cp -R /home/wilfordw/production_laravel_app/public/img/* /home/wilfordw/public_html/img
+    cp -R /home/wilfordw/production_laravel_app/public/files/* /home/wilfordw/public_html/files
+    cp -R /home/wilfordw/production_laravel_app/public/favicon/* /home/wilfordw/public_html/favicon
+    cp -R /home/wilfordw/production_laravel_app/public/vendor/* /home/wilfordw/public_html/vendor
+
     composer install
     php artisan migrate --force
+    php artisan route:cache
+    php artisan storage:link
 @endtask
