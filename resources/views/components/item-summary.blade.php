77dd81b81517f77452c31532659a821b97793ad2
Jon Fackrell <jon.fackrell@gmail.com>
2021-10-10 18:59:41 -0600

    Develop (#45)
    * Removed dupilcate subjects.show route name and unused routes to enable route caching
    
    * Changed LFM config to prevent duplicate routes
    
    * Only show people who have pages
    
    * only search enabled items
    
    * - Add single video page
    - Add slug to presses
    
    * Updated article route parameter binding to check for numeric instead of integer
    
    * Update people page search
    
    * no message
    
    * no message
    
    * no message
    
    * Updated job description
    Add options to Http for retrieving page data
    
    * Added Page export
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * Set add_to_collection_at when item has been enabled
    
    * Set added_to_collection_at when enabling
    
    * Remove webinar announcement
    Add lazy image loading

--- resources/views/components/item-summary.blade.php
+++ resources/views/components/item-summary.blade.php
@@ -1,7 +1,11 @@
 <li class="py-4 grid grid-cols-7">
     <div class="col-span-1 ">
         <a class="col-span-1" href="{{ route('documents.show', ['item' => $item]) }}">
-            <img class="h-auto w-20 my-2 mx-auto" src="{{ optional(optional($item->pages->first())->getFirstMedia())->getUrl('thumb') }}" alt="">
+            <img class="h-auto w-20 my-2 mx-auto"
+                 src="{{ optional(optional($item->pages->first())->getFirstMedia())->getUrl('thumb') }}"
+                 alt=""
+                 loading="lazy"
+            >
         </a>
     </div>
     <div class="col-span-6 py-2">
