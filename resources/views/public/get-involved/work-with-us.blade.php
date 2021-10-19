b6bb62f161c2a16e1fc03db74210d543c0d13168
Jon Fackrell <jon.fackrell@gmail.com>
2021-10-19 17:19:39 -0600

    Develop (#51)
    * Removed dupilcate subjects.show route name and unused routes to enable route caching
    
    * Changed LFM config to prevent duplicate routes
    
    * Only show people who have pages
    
    * only search enabled items
    
    * - Add single video page
    - Add slug to presses
    
    * Updated article route parameter binding to check for numeric instead of integer
    
    * Update people page search
    
    * no message
    
    * no message
    
    * no message
    
    * Updated job description
    Add options to Http for retrieving page data
    
    * Added Page export
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * Set add_to_collection_at when item has been enabled
    
    * Set added_to_collection_at when enabling
    
    * Remove webinar announcement
    Add lazy image loading
    
    * no message
    
    * no message
    
    * Add dynamic jobs
    
    * Fix submenu for jobs section
    
    * Fixed submenu for job opportunities

--- resources/views/public/get-involved/work-with-us.blade.php
+++ resources/views/public/get-involved/work-with-us.blade.php
@@ -4,7 +4,7 @@
             <div class="blocks">
                 <div class="grid grid-cols-12 py-12">
                     <div class="col-span-12 md:col-span-3 px-2 py-16">
-                        <x-submenu area="Media"/>
+                        <x-submenu area="Get Involved"/>
                     </div>
                     <div class="content col-span-12 md:col-span-9">
                         <h2>Job & Volunteer Opportunities</h2>
