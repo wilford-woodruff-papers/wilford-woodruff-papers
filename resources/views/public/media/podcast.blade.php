ee693b0050f3a434e5bb1d51344a1eadc55842b6
Jon Fackrell <jon.fackrell@gmail.com>
2021-12-31 10:53:23 -0700

    Develop (#67)
    * Removed dupilcate subjects.show route name and unused routes to enable route caching
    
    * Changed LFM config to prevent duplicate routes
    
    * Only show people who have pages
    
    * only search enabled items
    
    * - Add single video page
    - Add slug to presses
    
    * Updated article route parameter binding to check for numeric instead of integer
    
    * Update people page search
    
    * no message
    
    * no message
    
    * no message
    
    * Updated job description
    Add options to Http for retrieving page data
    
    * Added Page export
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * Set add_to_collection_at when item has been enabled
    
    * Set added_to_collection_at when enabling
    
    * Remove webinar announcement
    Add lazy image loading
    
    * no message
    
    * no message
    
    * Add dynamic jobs
    
    * Fix submenu for jobs section
    
    * Fixed submenu for job opportunities
    
    * Restrict places to subjects with pages
    
    * Don't clear media collection and only add media if id doesn't already exist
    
    * no message
    
    * removed nova
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * Fixed filemanager route
    
    * Revert "Fixed filemanager route"
    
    This reverts commit a02fb17e77fcc7ea48f78b8262e0d0526c23f927.
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * Updates to career and internships
    
    * no message
    
    * no message
    
    * Remove doc name
    
    * Changed news descriptions to excerpts
    
    * Added press type filter to Nova
    
    * Add option to reimport images
    
    * Modified trascript field on press model to hold full transcript and display on video page
    
    * no message
    
    * no message
    
    * Added transcipts to podcasts

---
+++ resources/views/public/media/podcast.blade.php
@@ -0,0 +1,56 @@
+<x-guest-layout>
+    <div id="content" role="main">
+        <div class="max-w-7xl mx-auto px-4">
+            <div class="blocks">
+                <div class="grid grid-cols-12 py-12">
+                    <div class="col-span-12 md:col-span-3 px-2 py-16">
+                        <x-submenu area="Media"/>
+                    </div>
+                    <div class="content col-span-12 md:col-span-9">
+                        <div class="mt-4 pt-0 px-4 grid gap-16 grid-cols-1 lg:gap-y-12">
+                            <div>
+                                <p class="text-sm text-gray-500">
+                                    <time datetime="{{ $podcast->date }}">{{ $podcast->date->format('j M Y') }}</time>
+                                </p>
+                                <a href="{{ $podcast->link }}" class="mt-2 block" target="_podcast">
+                                    <p class="text-xl font-semibold text-primary">
+                                        {{ $podcast->title }}
+                                    </p>
+                                    <p>
+                                        {{ $podcast->subtitle }}
+                                    </p>
+                                    <p class="mt-3 text-base text-gray-500">
+                                        {!! $podcast->description !!}
+                                    </p>
+                                </a>
+                                <div class="mt-3">
+                                    {!! $podcast->embed !!}
+                                </div>
+                                <div class="mt-3">
+                                    @if(empty($podcast->embed))
+                                        <a href="{{ $podcast->link }}"
+                                           class="text-base font-semibold text-secondary hover:text-highlight"
+                                           target="_podcast"
+                                        >
+                                            Listen now
+                                        </a>
+                                    @endif
+                                </div>
+                                @if(strlen($podcast->transcript) > 10)
+                                    <div class="p-6">
+                                        <p class="text-xl font-semibold text-gray-900">
+                                            Transcript
+                                        </p>
+                                        <div class="mt-8">
+                                            {!! $podcast->transcript !!}
+                                        </div>
+                                    </div>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-guest-layout>
