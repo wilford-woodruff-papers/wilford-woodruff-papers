229581bfae846941bdf71a77775fddefeffea675
Jon Fackrell <jon.fackrell@gmail.com>
2022-01-06 23:32:20 -0700

    Develop (#77)
    * Removed dupilcate subjects.show route name and unused routes to enable route caching
    
    * Changed LFM config to prevent duplicate routes
    
    * Only show people who have pages
    
    * only search enabled items
    
    * - Add single video page
    - Add slug to presses
    
    * Updated article route parameter binding to check for numeric instead of integer
    
    * Update people page search
    
    * no message
    
    * no message
    
    * no message
    
    * Updated job description
    Add options to Http for retrieving page data
    
    * Added Page export
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * Set add_to_collection_at when item has been enabled
    
    * Set added_to_collection_at when enabling
    
    * Remove webinar announcement
    Add lazy image loading
    
    * no message
    
    * no message
    
    * Add dynamic jobs
    
    * Fix submenu for jobs section
    
    * Fixed submenu for job opportunities
    
    * Restrict places to subjects with pages
    
    * Don't clear media collection and only add media if id doesn't already exist
    
    * no message
    
    * removed nova
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * Fixed filemanager route
    
    * Revert "Fixed filemanager route"
    
    This reverts commit a02fb17e77fcc7ea48f78b8262e0d0526c23f927.
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * Updates to career and internships
    
    * no message
    
    * no message
    
    * Remove doc name
    
    * Changed news descriptions to excerpts
    
    * Added press type filter to Nova
    
    * Add option to reimport images
    
    * Modified trascript field on press model to hold full transcript and display on video page
    
    * no message
    
    * no message
    
    * Added transcipts to podcasts
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * Database changes to wives and children
    
    * no message
    
    * no message
    
    * no message
    
    * Added subject export

---
+++ app/Nova/Filters/SubjectType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Nova\Filters;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\Select;
+use Laravel\Nova\Filters\Filter;
+
+class SubjectType extends Filter
+{
+    /**
+     * The filter's component.
+     *
+     * @var string
+     */
+    public $component = 'select-filter';
+
+    /**
+     * Apply the filter to the given query.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  mixed  $value
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function apply(Request $request, $query, $value)
+    {
+        if($value == -1){
+
+        }else {
+            $query = $query->whereHas('category', function (Builder $query) use ($value) {
+                $query->where('id', $value);
+            });
+        }
+        return $query;
+    }
+
+    /**
+     * Get the filter's available options.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function options(Request $request)
+    {
+        return ['Not Set' => -1] + \App\Models\Category::orderBy('name')->get()->pluck('id', 'name')->all();
+    }
+}
