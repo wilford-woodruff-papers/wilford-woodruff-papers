2aea5dd1a21b497cb8af2f65c53be1d1d7d22c17
Jon Fackrell <jon.fackrell@gmail.com>
2022-01-13 09:04:32 -0700

    Hide team if it doesn't have board members

--- resources/views/public/about/meet-the-team.blade.php
+++ resources/views/public/about/meet-the-team.blade.php
@@ -10,79 +10,82 @@
                         <h2>Meet the Team</h2>
                         <div class="grid grid-cols-1 md:grid-cols-2">
                             @foreach($teams as $team)
-                                <div class="col-span-1">
-                                    <h3 class="text-primary text-2xl font-serif mt-4 mb-8 border-b border-gray-300">
-                                        {{ $team->name }}
-                                    </h3>
-                                    <div class="divide-y divide-gray-200">
-                                        @foreach($team->boardmembers->sortBy('order') as $person)
-                                            <div class="pt-4 pb-4 flex items-center">
-                                                <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
-                                                </svg>
-                                                <a href="#{{ Str::of($person->name)->slug() }}" class="ml-3 cursor-pointer">
-                                                <span class="text-base font-medium text-secondary">
-                                                    {{ $person->name }}@if(! empty($person->title)), {{ $person->title }}@endif
-                                                </span>
-                                                </a>
-                                            </div>
-                                        @endforeach
+                                @if($team->boardmembers->count() > 0)
+                                    <div class="col-span-1">
+                                        <h3 class="text-primary text-2xl font-serif mt-4 mb-8 border-b border-gray-300">
+                                            {{ $team->name }}
+                                        </h3>
+                                        <div class="divide-y divide-gray-200">
+                                            @foreach($team->boardmembers->sortBy('order') as $person)
+                                                <div class="pt-4 pb-4 flex items-center">
+                                                    <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+                                                    </svg>
+                                                    <a href="#{{ Str::of($person->name)->slug() }}" class="ml-3 cursor-pointer">
+                                                    <span class="text-base font-medium text-secondary">
+                                                        {{ $person->name }}@if(! empty($person->title)), {{ $person->title }}@endif
+                                                    </span>
+                                                    </a>
+                                                </div>
+                                            @endforeach
+                                        </div>
                                     </div>
-                                </div>
+                                @endif
                             @endforeach
                         </div>
 
                         <div class="grid grid-cols-1 gap-x-4 mt-12">
                             @foreach($teams as $team)
-                                <div class="h-16">
-                                    <div class="max-w-7xl mx-auto px-4">
-                                        <div class="relative">
-                                            <div class="absolute inset-0 flex items-center" aria-hidden="true">
-                                                <div class="w-full border-t-2 border-gray-300" style="height: 0px"></div>
-                                            </div>
-                                            <div class="relative flex justify-center">
-                                                <div class="inline-flex items-center shadow-sm px-8 py-4 border-2 border-gray-300 text-4xl leading-5 font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                                    <h1 class="uppercase">{{ $team->name }}</h1>
+                                @if($team->boardmembers->count() > 0)
+                                    <div class="h-16">
+                                        <div class="max-w-7xl mx-auto px-4">
+                                            <div class="relative">
+                                                <div class="absolute inset-0 flex items-center" aria-hidden="true">
+                                                    <div class="w-full border-t-2 border-gray-300" style="height: 0px"></div>
+                                                </div>
+                                                <div class="relative flex justify-center">
+                                                    <div class="inline-flex items-center shadow-sm px-8 py-4 border-2 border-gray-300 text-4xl leading-5 font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                                        <h1 class="uppercase">{{ $team->name }}</h1>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
 
-                                @foreach($team->boardmembers->sortBy('order') as $person)
-                                    <div class="mt-12 px-8 md:px-2">
-                                        <div class="space-y-12 sm:divide-y sm:divide-gray-200 sm:space-y-0 sm:-mt-8 lg:gap-x-8 lg:space-y-0" x-max="1">
+                                    @foreach($team->boardmembers->sortBy('order') as $person)
+                                        <div class="mt-12 px-8 md:px-2">
+                                            <div class="space-y-12 sm:divide-y sm:divide-gray-200 sm:space-y-0 sm:-mt-8 lg:gap-x-8 lg:space-y-0" x-max="1">
 
-                                            <div class="sm:py-8"
-                                                 id="{{ Str::of($person->name)->slug() }}"
-                                            >
-                                                <div class="flex items-center space-y-4 sm:grid sm:grid-cols-3 sm:items-start sm:gap-6 sm:space-y-0">
-                                                    <!-- Image -->
-                                                    <div class="aspect-w-3 aspect-h-2 sm:aspect-w-3 sm:aspect-h-4 hidden md:block">
-                                                        <img class="object-cover shadow-lg rounded-full h-80 w-80" src="{{ Storage::disk('board_members')->url($person->image) }}" alt="">
-                                                    </div>
-                                                    <div class="col-span-3 md:col-span-2">
-                                                        <div class="space-y-4">
-                                                            <div class="text-lg leading-6 font-medium space-y-1">
-                                                                <h3 class="text-3xl text-primary font-semibold serif"
-                                                                >{{ $person->name }}</h3>
-                                                                @if(! empty($person->title))
-                                                                    <p class="text-gray-600 uppercase serif">{{ $person->title }}</p>
-                                                                @endif
-                                                            </div>
-                                                            <div class="text-lg">
-                                                                <img class="object-cover shadow-lg rounded-full h-40 w-40 md:hidden block" src="{{ Storage::disk('board_members')->url($person->image) }}" alt="">
-                                                                <p class="text-gray-500">{!! $person->bio !!}</p>
+                                                <div class="sm:py-8"
+                                                     id="{{ Str::of($person->name)->slug() }}"
+                                                >
+                                                    <div class="flex items-center space-y-4 sm:grid sm:grid-cols-3 sm:items-start sm:gap-6 sm:space-y-0">
+                                                        <!-- Image -->
+                                                        <div class="aspect-w-3 aspect-h-2 sm:aspect-w-3 sm:aspect-h-4 hidden md:block">
+                                                            <img class="object-cover shadow-lg rounded-full h-80 w-80" src="{{ Storage::disk('board_members')->url($person->image) }}" alt="">
+                                                        </div>
+                                                        <div class="col-span-3 md:col-span-2">
+                                                            <div class="space-y-4">
+                                                                <div class="text-lg leading-6 font-medium space-y-1">
+                                                                    <h3 class="text-3xl text-primary font-semibold serif"
+                                                                    >{{ $person->name }}</h3>
+                                                                    @if(! empty($person->title))
+                                                                        <p class="text-gray-600 uppercase serif">{{ $person->title }}</p>
+                                                                    @endif
+                                                                </div>
+                                                                <div class="text-lg">
+                                                                    <img class="object-cover shadow-lg rounded-full h-40 w-40 md:hidden block" src="{{ Storage::disk('board_members')->url($person->image) }}" alt="">
+                                                                    <p class="text-gray-500">{!! $person->bio !!}</p>
+                                                                </div>
                                                             </div>
                                                         </div>
                                                     </div>
                                                 </div>
+
                                             </div>
-
                                         </div>
-                                    </div>
-                                @endforeach
-
+                                    @endforeach
+                                @endif
                             @endforeach
                         </div>
                     </div>
