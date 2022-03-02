9e2853b143a094c2a427852ef883366ef5065774
Jon Fackrell <jon.fackrell@gmail.com>
2022-03-02 13:29:28 -0700

    disable primary key requirement for sesison and add id to password_resets table (#105)

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreatePasswordResetsTable extends Migration
@@ -13,7 +14,9 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
+        DB::statement('SET SESSION sql_require_primary_key=0');
         Schema::create('password_resets', function (Blueprint $table) {
+            $table->id();
             $table->string('email')->index();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
