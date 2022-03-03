ecb0a882dd640846884502cdad2230148cff176a
Jon Fackrell <jon.fackrell@gmail.com>
2022-03-03 14:48:44 -0700

    Removed 'faithful' from mission statement and separated our purpose.

--- resources/views/components/mission-statement.blade.php
+++ resources/views/components/mission-statement.blade.php
@@ -1,6 +1,4 @@
 <div class="mb-8">
-    <p class="text-primary text-lg">Our mission, by 2030, is to digitally preserve and publish Wilford Woodruff’s faithful eyewitness account of the Restoration of the Gospel of Jesus Christ from 1833 to 1898.</p>
+    <p class="text-primary text-lg">Our mission, by 2030, is to digitally preserve and publish Wilford Woodruff’s eyewitness account of the Restoration of the Gospel of Jesus Christ from 1833 to 1898.</p>
 </div>
-<div class="mb-8">
-    <p class="text-primary text-lg">Our purpose in making Wilford Woodruff’s faithful records universally accessible is to inspire all people, especially the rising generation, to study and increase their faith in Jesus Christ, understand and honor sacred temple covenants, and thereby receive the blessings of exaltation with their families.</p>
-</div>
+<x-purpose />
