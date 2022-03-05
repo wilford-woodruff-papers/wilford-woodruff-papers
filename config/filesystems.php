14c972110d3e8b2998aec11af5b805056365277e
Jon Fackrell <jon.fackrell@gmail.com>
2022-03-04 17:14:12 -0700

    Add spaces driver

--- config/filesystems.php
+++ config/filesystems.php
@@ -80,6 +80,18 @@ return [
             'endpoint' => env('AWS_ENDPOINT'),
         ],
 
+        'spaces' => [
+            'driver' => 's3',
+            'key' => env('DO_SPACES_KEY'),
+            'secret' => env('DO_SPACES_SECRET'),
+            'region' => env('DO_SPACES_REGION'),
+            'bucket' => env('DO_SPACES_BUCKET'),
+            'url' => env('DO_SPACES_URL'),
+            'endpoint' => env('DO_SPACES_ENDPOINT'),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'visibility' => 'public',
+        ],
+
     ],
 
     /*
