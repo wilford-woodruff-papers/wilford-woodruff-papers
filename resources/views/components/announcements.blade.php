911720c11d227c7dd467b39bd0d87c676642a254
Jon Fackrell <jon.fackrell@gmail.com>
2022-03-07 17:42:53 -0700

    no message

--- resources/views/components/announcements.blade.php
+++ resources/views/components/announcements.blade.php
@@ -1,5 +1,9 @@
 <div>
     @foreach($announcements as $announcement)
-        @include('announcements.single', ['announcement' => $announcement])
+        <div class="bg-white mt-4 mb-8">
+            <div class="max-w-7xl mx-auto pt-8 md:pt-16 px-12 pb-4 xl:pt-16  md:px-24 md:pb-8">
+                @include('announcements.single', ['announcement' => $announcement])
+            </div>
+        </div>
     @endforeach
 </div>
