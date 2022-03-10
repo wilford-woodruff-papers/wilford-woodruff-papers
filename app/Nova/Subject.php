fc92383d3275c7405a13df3dd9727c2295de3077
Jon Fackrell <jon.fackrell@gmail.com>
2022-03-09 17:00:41 -0700

    no message

--- app/Nova/Subject.php
+++ app/Nova/Subject.php
@@ -7,7 +7,9 @@ use App\Nova\Actions\ImportBiographies;
 use App\Nova\Actions\ImportSubjects;
 use App\Nova\Filters\SubjectType;
 use Illuminate\Http\Request;
+use Laravel\Nova\Fields\BelongsTo;
 use Laravel\Nova\Fields\BelongsToMany;
+use Laravel\Nova\Fields\HasMany;
 use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Http\Requests\NovaRequest;
@@ -52,6 +54,10 @@ class Subject extends Resource
             Text::make(__('Name'), 'name')->sortable(),
             Text::make(__('Slug'), 'slug')->hideWhenCreating()->sortable(),
             BelongsToMany::make('Category'),
+            BelongsTo::make('Parent Subject', 'parent', Subject::class)
+                ->nullable()
+                ->searchable(),
+            HasMany::make('Subjects', 'children', Subject::class),
         ];
     }
 
