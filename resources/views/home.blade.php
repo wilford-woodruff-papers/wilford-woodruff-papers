35e7da559957055ab9b63cff6f211a2e3c4d13df
Jon Fackrell <jon.fackrell@gmail.com>
2022-03-14 09:56:45 -0600

    no message

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -37,45 +37,49 @@
         <x-article-preview-carousel />
     @endif
 
-    @if(! empty($article))
-        <div class="bg-white">
-            <div class="max-w-7xl mx-auto pt-4 md:pt-4 px-12 pb-4 xl:pt-4 md:px-24 md:pb-8">
-                <div class="">
-                    <p class="uppercase text-secondary font-semibold">
-                        Article
-                    </p>
-                    <h2 class="text-4xl">
-                        {{ $article->title }}
-                    </h2>
-                    <p class="text-gray-600">
-                        {{ $article->date->toFormattedDateString() }}
-                    </p>
-                    <p class="text-base my-4">
-                        @if(! empty($article->excerpt))
-                            {!! $article->excerpt !!}
-                        @else
-                            {!! \Illuminate\Support\Str::of(strip_tags($article->description))->limit(500, ' ...') !!}
-                        @endif
-                    </p>
-                    <a href="{{ route('media.article', ['article' => $article]) }}"
-                       class="text-secondary font-semibold">
-                        Read more &gt;
-                    </a>
+    @if(app()->environment(['production']))
+        @if(! empty($article))
+            <div class="bg-white">
+                <div class="max-w-7xl mx-auto pt-4 md:pt-4 px-12 pb-4 xl:pt-4 md:px-24 md:pb-8">
+                    <div class="">
+                        <p class="uppercase text-secondary font-semibold">
+                            Article
+                        </p>
+                        <h2 class="text-4xl">
+                            {{ $article->title }}
+                        </h2>
+                        <p class="text-gray-600">
+                            {{ $article->date->toFormattedDateString() }}
+                        </p>
+                        <p class="text-base my-4">
+                            @if(! empty($article->excerpt))
+                                {!! $article->excerpt !!}
+                            @else
+                                {!! \Illuminate\Support\Str::of(strip_tags($article->description))->limit(500, ' ...') !!}
+                            @endif
+                        </p>
+                        <a href="{{ route('media.article', ['article' => $article]) }}"
+                           class="text-secondary font-semibold">
+                            Read more &gt;
+                        </a>
+                    </div>
+                </div>
+            </div>
+        @endif
+    @endif
+
+    @if(app()->environment(['production']))
+        <div class="bg-primary">
+            <div class="max-w-7xl mx-auto pt-8 md:pt-16 px-12 pb-4 xl:pt-24 md:px-36 md:pb-8">
+                <div class="text-3xl md:text-4xl xl:text-5xl text-justify text-highlight pb-4 leading-10" style="font-family: 'Italianno', cursive;">
+                    " We pray that thou wilt bring to our remembrance all things which are necessary to the writing of this history . . . that when we have gone into the world of spirits that the saints of God may be blessed in reading our record which we have kept."
+                </div>
+                <div class="text-xl text-highlight italic text-center">
+                    -- Wilford Woodruff
                 </div>
             </div>
         </div>
     @endif
-
-    <div class="bg-primary">
-        <div class="max-w-7xl mx-auto pt-8 md:pt-16 px-12 pb-4 xl:pt-24 md:px-36 md:pb-8">
-            <div class="text-3xl md:text-4xl xl:text-5xl text-justify text-highlight pb-4 leading-10" style="font-family: 'Italianno', cursive;">
-                " We pray that thou wilt bring to our remembrance all things which are necessary to the writing of this history . . . that when we have gone into the world of spirits that the saints of God may be blessed in reading our record which we have kept."
-            </div>
-            <div class="text-xl text-highlight italic text-center">
-                -- Wilford Woodruff
-            </div>
-        </div>
-    </div>
 
     @if(app()->environment(['local','development']))
         <x-home.book />
@@ -83,15 +87,16 @@
         <x-home.video />
     @endif
 
-    <div class="mt-4 md:my-12">
-        <div class="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-4 xl:grid-cols-2 gap-2 px-4 md:px-12 md:gap-4 md:px-40">
-            <x-polaroid route="documents" image="img/home/documents.jpg" name="Documents" />
-            <x-polaroid route="people" image="img/home/people.jpg" name="People" />
-            <x-polaroid route="places" image="img/home/places.jpg" name="Places" />
-            <x-polaroid route="timeline" image="img/home/historical-context.jpg" name="Timeline" />
+    @if(app()->environment(['production']))
+        <div class="mt-4 md:my-12">
+            <div class="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-4 xl:grid-cols-2 gap-2 px-4 md:px-12 md:gap-4 md:px-40">
+                <x-polaroid route="documents" image="img/home/documents.jpg" name="Documents" />
+                <x-polaroid route="people" image="img/home/people.jpg" name="People" />
+                <x-polaroid route="places" image="img/home/places.jpg" name="Places" />
+                <x-polaroid route="timeline" image="img/home/historical-context.jpg" name="Timeline" />
+            </div>
         </div>
-    </div>
-
+    @endif
     {{--@push('styles')
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Source+Serif+Pro&display=swap" rel="stylesheet">
