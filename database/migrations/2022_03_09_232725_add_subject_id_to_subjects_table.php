88ee6e7a0584f9aa6af6843179f0fd976f087642
Jon Fackrell <jon.fackrell@gmail.com>
2022-03-14 12:58:09 -0600

    no message

--- database/migrations/2022_03_09_232725_add_subject_id_to_subjects_table.php
+++ database/migrations/2022_03_09_232725_add_subject_id_to_subjects_table.php
@@ -14,7 +14,7 @@ class AddSubjectIdToSubjectsTable extends Migration
     public function up()
     {
         Schema::table('subjects', function (Blueprint $table) {
-            $table->foreignIdFor(\App\Models\Subject::class);
+            $table->foreignIdFor(\App\Models\Subject::class)->nullable();
         });
     }
 
