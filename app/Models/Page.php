e454b421927fdc3c0dfe9b41ce915dcd7c37ce7e
Jon Fackrell <jon.fackrell@gmail.com>
2022-03-17 12:42:41 -0600

    no message

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -61,7 +61,7 @@ class Page extends Model implements HasMedia, \OwenIt\Auditing\Contracts\Auditab
         } elseif ($this->dates()->get()->count() > 1) {
             return $this->dates()->get()->first()->date->format('F j, Y') . ' - ' .  $this->dates()->get()->last()->date->format('F j, Y');
         }
-        if($page = Page::has('dates')->where('parent_item_id', $this->parent_item_id)->where('order', '<', $this->order)->orderBy('order', 'ASC')->first()){
+        if($page = Page::has('dates')->where('parent_item_id', $this->parent_item_id)->where('order', '<', $this->order)->orderBy('order', 'DESC')->first()){
             return $page->dates()->get()->sortBy('date')->first()->date->format('F j, Y');
         } else {
             return 'p. ' . $this->order;
