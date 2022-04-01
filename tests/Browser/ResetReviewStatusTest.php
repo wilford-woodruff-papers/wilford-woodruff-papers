4c19c8da186e40083698916367a7ba19f01ade79
Jon Fackrell <jon.fackrell@gmail.com>
2022-04-01 12:42:39 -0600

    no message

--- tests/Browser/ResetReviewStatusTest.php
+++ tests/Browser/ResetReviewStatusTest.php
@@ -20,35 +20,55 @@ class ResetReviewStatusTest extends DuskTestCase
      */
     public function testResetLetters()
     {
+        set_time_limit(7200);
+
         Role::create(['name'=> 'Super Admin']);
 
-        $this->browse(function (Browser $browser) {
+
+
+        $this->browse(function (Browser $browser){
+
             $browser->visit('https://fromthepage.com/users/sign_in')
                     ->type('user[login_id]', env('FTP_USER'))
                     ->type('user[password]', env('FTP_PASSWORD'))
-                    ->press('Sign In');
+                    ->press('Sign In')
+                    ->pause(3000);
 
-            $items = collect(json_decode(file_get_contents(__DIR__ . '/Fixtures/970.json'), true)['manifests']);
+            // $items = collect(json_decode(file_get_contents(__DIR__ . '/Fixtures/970.json'), true)['manifests']);
+            $items = collect(Http::get('https://fromthepage.com/iiif/collection/970')->json('manifests', []));
 
-            $items->each(function($item) use ($browser){
+            foreach($items as $item){
                 if( Str::of($item['label'])->startsWith('Letter')
                     && $item['service']['pctComplete'] == 100.0
                 ){
-                    $pages = collect(Http::get($item['@id'])->json('sequences.0.canvases', []));
-                    ray($pages);
-                    $pages->each(function($page) use ($browser){
-                        $id = str($page['@id'])->afterLast('/');
-                        $transcriptionLink = collect($page['related'])->where('label', 'Transcribe this page')->first()['@id'];
+                    logger()->info($item['label']);
+                    $pages = collect(Http::timeout(60)
+                        ->connectTimeout(60)
+                        ->retry(3, 2000)
+                        ->get($item['@id'])
+                        ->json('sequences.0.canvases', []));
 
-                        $browser->visit($transcriptionLink)
-                                ->assertSee('Transcribe')
-                                ->press('Save')
-                                ->screenshot($id);
+                    try{
+                        foreach($pages as $page) {
+                            $id = str($page['@id'])->afterLast('/');
+                            $transcriptionLink = collect($page['related'])->where('label', 'Transcribe this page')->first()['@id'];
+                            logger()->info($transcriptionLink);
+                            $browser->visit($transcriptionLink)
+                                    ->pause(200)
+                                    ->with('.page-toolbar', function ($toolbar) {
+                                        $toolbar->press('Save');
+                                    })
+                                    ->pause(1000);
+                                //->waitForText('Saved', 30)
+                                //->screenshot($id)
+                                //->pause(2000);
+                        }
+                    }catch(\Exception $exception){
+                        logger()->info('Failed: '.$exception->getMessage());
+                    }
 
-                    });
-                    dd('Stop');
                 }
-            });
+            }
         });
     }
 }
