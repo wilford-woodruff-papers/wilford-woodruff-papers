8debecd15caacf4de29dcd8aa09c9940e0121c5c
Jon Fackrell <jon.fackrell@gmail.com>
2022-04-13 15:26:23 -0600

    no message

--- resources/views/components/home/progress.blade.php
+++ resources/views/components/home/progress.blade.php
@@ -8,6 +8,20 @@
     </div>
     <div class="">
         <div class="max-w-7xl mx-auto px-4 py-4 md:px-4 md:py-4">
+            <div class="mb-2">
+                <div class="max-w-7xl mx-auto px-2 pb-4 md:pb-8">
+                    <div class="flex flex-col shadow-lg overflow-hidden">
+                        <div class="flex-shrink-0">
+                            <div class="max-w-full md:max-w-7xl h-auto mx-auto">
+                                <a href="https://wilfordwoodruffpapers.org/announcements/project-progress-march-1-2022"
+                                   target="_blank">
+                                    <img src="https://nyc3.digitaloceanspaces.com/wilford-woodruff-papers/img/quarterly-progress-report.png" alt="Quarterly Progress Report"/>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div class="grid md:grid-cols-2 gap-5">
                 <div class="px-2 mx-auto">
                     <img class="object-cover object-center shadow-2xl" src="https://nyc3.digitaloceanspaces.com/wilford-woodruff-papers/img/progress-1.png" alt="">
