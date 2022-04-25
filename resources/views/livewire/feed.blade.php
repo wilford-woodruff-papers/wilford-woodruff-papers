14d30936dc732ce5d8ebeefa5c1ab6ccedb5301b
Jon Fackrell <jon.fackrell@gmail.com>
2022-04-25 15:42:36 -0600

    no message

--- resources/views/livewire/feed.blade.php
+++ resources/views/livewire/feed.blade.php
@@ -24,7 +24,7 @@
                                 {{--<img class="h-48 w-full object-cover"
                                      src="{{ \Illuminate\Support\Facades\Storage::disk('media')->url($media->cover_image)  }}"
                                      alt="{{ $media->title }}">--}}
-                                <div class="image-parent relative h-auto w-full overflow-hidden inline-block flex items-center bg-primary-50">
+                                <div class="image-parent relative h-80 w-full overflow-hidden inline-block flex items-center bg-primary-50">
                                     <div class="image-child absolute h-full w-full z-10 bg-cover bg-center z-0" style="background-image: url({{ \Illuminate\Support\Facades\Storage::disk('media')->url($media->cover_image)  }})">
 
                                     </div>
@@ -76,7 +76,7 @@
                                         </svg>
                                     </div>
                                     <div>
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
                                         </svg>
                                     </div>
