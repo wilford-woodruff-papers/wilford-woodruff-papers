99e279d81fec3d18280ef857d091c37811696ff8
Jon Fackrell <jon.fackrell@gmail.com>
2022-04-25 14:44:37 -0600

    no message

--- resources/views/public/get-involved/index.blade.php
+++ resources/views/public/get-involved/index.blade.php
@@ -9,21 +9,43 @@
                     <div class="content col-span-12 md:col-span-9">
                         <h2>Get Involved</h2>
 
-                        <div class="mt-4 grid gap-16 lg:grid-cols-1 lg:gap-y-12">
+                        <div class="mt-4 grid gap-2 lg:grid-cols-1 lg:gap-y-2">
                             There are several ways you can get involved to help us complete the mission and purpose of the Wilford Woodruff Papers Foundation.
 
-                            <p>
-                                <a href="{{ route('volunteer') }}"
-                                   class="text-secondary font-medium">
-                                    Volunteer to help transcribe >>
-                                </a>
-                            </p>
-                            <p>
-                                <a href="http://wwp.test/work-with-us/internship-opportunities"
-                                   class="text-secondary font-medium">
-                                    Volunteer to help transcribe >>
-                                </a>
-                            </p>
+                            <div>
+                                <p>
+                                    We are so grateful to have volunteers who share their time and talents with the Wilford Woodruff Papers Project. To learn more about available volunteer opportunities, please share your contact information and we'll reach out to you as soon as possible.
+                                </p>
+                                <p>
+                                    <a href="{{ route('volunteer') }}"
+                                       class="text-secondary font-medium">
+                                        Volunteer >>
+                                    </a>
+                                </p>
+                            </div>
+                            <div>
+                                <p>
+                                    The Wilford Woodruff Papers Foundation makes several internships available each semester to college and high school students through a generous grant from the Sorenson Legacy Foundation. Our interns help with everything from publishing Wilford Woodruff's documents to publishing socila media posts.
+                                </p>
+                                <p>
+                                    <a href="http://wwp.test/work-with-us/internship-opportunities"
+                                       class="text-secondary font-medium">
+                                        Apply for an Internship >>
+                                    </a>
+                                </p>
+                            </div>
+
+                            <div>
+                                <p>
+                                    Occasionally we hire people to fill part or full-time positions within the organization.
+                                </p>
+                                <p>
+                                    <a href="{{ route('work-with-us') }}"
+                                       class="text-secondary font-medium">
+                                        Apply for a Job >>
+                                    </a>
+                                </p>
+                            </div>
                         </div>
 
                     </div>
