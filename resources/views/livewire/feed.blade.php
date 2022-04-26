9932721e10b07d6f90f383f8c1b55dab9b8b5380
Jon Fackrell <jon.fackrell@gmail.com>
2022-04-26 08:57:43 -0600

    no message

--- resources/views/livewire/feed.blade.php
+++ resources/views/livewire/feed.blade.php
@@ -132,14 +132,20 @@
                                                 <div class="border-t border-gray-400 px-2">
                                                     <div class="flex gap-x-1 items-center px-6 hover:bg-gray-100">
                                                         <svg fill="#000000" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 50 50" class="w-6 h-6"><path d="M 50.0625 10.4375 C 48.214844 11.257813 46.234375 11.808594 44.152344 12.058594 C 46.277344 10.785156 47.910156 8.769531 48.675781 6.371094 C 46.691406 7.546875 44.484375 8.402344 42.144531 8.863281 C 40.269531 6.863281 37.597656 5.617188 34.640625 5.617188 C 28.960938 5.617188 24.355469 10.21875 24.355469 15.898438 C 24.355469 16.703125 24.449219 17.488281 24.625 18.242188 C 16.078125 17.8125 8.503906 13.71875 3.429688 7.496094 C 2.542969 9.019531 2.039063 10.785156 2.039063 12.667969 C 2.039063 16.234375 3.851563 19.382813 6.613281 21.230469 C 4.925781 21.175781 3.339844 20.710938 1.953125 19.941406 C 1.953125 19.984375 1.953125 20.027344 1.953125 20.070313 C 1.953125 25.054688 5.5 29.207031 10.199219 30.15625 C 9.339844 30.390625 8.429688 30.515625 7.492188 30.515625 C 6.828125 30.515625 6.183594 30.453125 5.554688 30.328125 C 6.867188 34.410156 10.664063 37.390625 15.160156 37.472656 C 11.644531 40.230469 7.210938 41.871094 2.390625 41.871094 C 1.558594 41.871094 0.742188 41.824219 -0.0585938 41.726563 C 4.488281 44.648438 9.894531 46.347656 15.703125 46.347656 C 34.617188 46.347656 44.960938 30.679688 44.960938 17.09375 C 44.960938 16.648438 44.949219 16.199219 44.933594 15.761719 C 46.941406 14.3125 48.683594 12.5 50.0625 10.4375 Z"/></svg>
-                                                        <a href="#" class="block w-full px-4 py-2 text-left text-sm" >
+                                                        <a href="https://twitter.com/share?text={{ $media->title }}&via=subjecttoclimat&url={{ route('media.'.Str::of($media->type)->lower(), $media->slug) }}"
+                                                           class="block w-full px-4 py-2 text-left text-sm"
+                                                           target="_blank"
+                                                        >
                                                             Twitter
                                                         </a>
                                                     </div>
 
                                                     <div class="flex gap-x-1 items-center px-6 hover:bg-gray-100">
                                                         <svg fill="#000000" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 50 50" class="w-6 h-6">    <path d="M41,4H9C6.24,4,4,6.24,4,9v32c0,2.76,2.24,5,5,5h32c2.76,0,5-2.24,5-5V9C46,6.24,43.76,4,41,4z M37,19h-2c-2.14,0-3,0.5-3,2 v3h5l-1,5h-4v15h-5V29h-4v-5h4v-3c0-4,2-7,6-7c2.9,0,4,1,4,1V19z"/></svg>
-                                                        <a href="#" disabled class="block w-full px-4 py-2 text-left text-sm">
+                                                        <a href="https://facebook.com/sharer.php?u={{ route('media.'.Str::of($media->type)->lower(), $media->slug) }}"
+                                                           class="block w-full px-4 py-2 text-left text-sm"
+                                                           target="_blank"
+                                                        >
                                                             Facebook
                                                         </a>
                                                     </div>
@@ -148,7 +154,9 @@
                                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                                             <path stroke-linecap="round" stroke-linejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                                                         </svg>
-                                                        <a href="#" disabled class="block w-full px-4 py-2 text-left text-sm">
+                                                        <a href="mailto:?subject={{ $media->title }}&body={{ route('media.'.Str::of($media->type)->lower(), $media->slug) }}"
+                                                           class="block w-full px-4 py-2 text-left text-sm"
+                                                           target="_blank">
                                                             Email
                                                         </a>
                                                     </div>
