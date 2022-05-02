60a8d4b1b8cc0bb773ee5160d914f2ccbe626cce
Jon Fackrell <jon.fackrell@gmail.com>
2022-05-02 13:48:52 -0600

    no message

--- app/Nova/Actions/ImportPages.php
+++ app/Nova/Actions/ImportPages.php
@@ -162,7 +162,7 @@ class ImportPages extends Action
         $links = $dom->find('a');
         foreach ($links as $link) {
             //dd($link->outerHtml(), $link->getAttribute('title'), $link->innerHtml());
-            $transcript = $transcript->replace($link->outerHtml(), '[['.html_entity_decode($link->getAttribute('title')).'|'.$link->innerHtml().']]');
+            $transcript = $transcript->replace(str($link->outerHtml())->replace('<br /> ', "<br/>\n"), '[['.html_entity_decode($link->getAttribute('title')).'|'.$link->innerHtml().']]');
         }
 
         return $transcript;
