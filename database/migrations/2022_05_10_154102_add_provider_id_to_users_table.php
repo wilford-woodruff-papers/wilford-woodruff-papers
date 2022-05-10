b5932f1d4e0aa95a4f8ecfe6702613cf4ab82a27
Jon Fackrell <jon.fackrell@gmail.com>
2022-05-10 09:45:42 -0600

    Develop (#184)

---
+++ database/migrations/2022_05_10_154102_add_provider_id_to_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('provider');
+        });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('provider_id')->after('profile_photo_path')->nullable()->index();
+            $table->string('provider')->after('profile_photo_path')->default('email')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('provider_id');
+            $table->dropColumn('provider');
+        });
+    }
+};
