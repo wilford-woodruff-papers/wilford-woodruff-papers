9d0e0f249c40dd88a8ba05932dd9442b507255a9
Jon Fackrell <jon.fackrell@gmail.com>
2022-05-27 10:34:39 -0600

    no message

---
+++ app/Console/Commands/IndexPageTopicsCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Page;
+use App\Models\Subject;
+use Illuminate\Console\Command;
+use Illuminate\Database\Eloquent\Builder;
+
+class IndexPageTopicsCommand extends Command
+{
+    protected $signature = 'index:topics';
+
+    protected $description = 'Index all topics for pages';
+
+    public function handle()
+    {
+        $topics = Subject::query()
+                    ->whereHas('category', function (Builder $query) {
+                        $query->where('name', 'Index');
+                    });
+
+        foreach($topics as $topic){
+            $pages = Page::where('transcript', 'LIKE', '%'. $topic->name .'%')->get();
+
+            foreach($pages as $page){
+                $page->subjects()->syncWithoutDetaching($topic->id);
+            }
+        }
+    }
+}
