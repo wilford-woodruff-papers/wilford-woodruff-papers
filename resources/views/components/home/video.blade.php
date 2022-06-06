eff98bba08e10d6dce72db1e1b2b3f370038d42e
Jon Fackrell <jon.fackrell@gmail.com>
2022-06-06 15:01:09 -0600

    no message

--- resources/views/components/home/video.blade.php
+++ resources/views/components/home/video.blade.php
@@ -1,6 +1,6 @@
 <div class="max-w-7xl mx-auto pt-4 md:pt-4 px-4 pb-4 xl:pt-4 md:pb-8">
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 pb-4">
-        <div class="font-extrabold">
+        <div class="font-extrabold col-span-2">
             <h2 class="text-3xl uppercase pb-1 border-b-4 border-highlight">What is the Wilford Woodruff Papers Project?</h2>
         </div>
     </div>
