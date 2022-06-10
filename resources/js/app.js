7bed03247f6be34b02ba28886925d6d42f6e8549
Jon Fackrell <jon.fackrell@gmail.com>
2022-06-10 16:29:30 -0600

    Re-styling of FAQs

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ require('./bootstrap');
 
 import Alpine from 'alpinejs'
 import intersect from '@alpinejs/intersect'
+import collapse from '@alpinejs/collapse'
 
 window.rangy = require('./rangy-core');
 window.classapplier = require('./rangy-classapplier');
@@ -9,6 +10,7 @@ window.highlighter = require('./rangy-highlighter');
 
 window.Alpine = Alpine
 Alpine.plugin(intersect)
+Alpine.plugin(collapse)
 Alpine.start()
 
 // A function is used for dragging and moving
