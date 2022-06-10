8f7881ece5a29ad5c974774eb4b1c8e71d402528
Jon Fackrell <jon.fackrell@gmail.com>
2022-06-10 14:30:44 -0600

    Update mission statement text style

--- resources/views/components/purpose.blade.php
+++ resources/views/components/purpose.blade.php
@@ -1,3 +1,3 @@
 <div class="mb-8">
-    <p class="text-primary text-lg">Our purpose in making Wilford Woodruff’s faithful records universally accessible is to inspire all people, especially the rising generation, to study and increase their faith in Jesus Christ, understand and honor sacred temple covenants, and thereby receive the blessings of exaltation with their families.</p>
+    <p class="{{ $fontStyle ?? 'text-primary text-lg' }}">Our purpose in making Wilford Woodruff’s faithful records universally accessible is to inspire all people, especially the rising generation, to study and increase their faith in Jesus Christ, understand and honor sacred temple covenants, and thereby receive the blessings of exaltation with their families.</p>
 </div>
