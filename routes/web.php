ff14e0cf79f86f624becf27b0b5e63873e23cad8
Jon Fackrell <jon.fackrell@gmail.com>
2022-06-17 16:25:32 -0600

    no message

--- routes/web.php
+++ routes/web.php
@@ -158,30 +158,33 @@ Route::get('/s/wilford-woodruff-papers/page/frequently-asked-questions', functio
     return redirect()->route('about.frequently-asked-questions');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])
-    ->get('/admin/dashboard/quotes', [\App\Http\Controllers\Admin\QuoteController::class, 'index'])
-    ->name('admin.dashboard.quotes.index');
+Route::group(['middleware' => ['role:Super Admin|Editor']], function () {
+    Route::middleware(['auth:sanctum', 'verified'])
+        ->get('/admin/dashboard/quotes', [\App\Http\Controllers\Admin\QuoteController::class, 'index'])
+        ->name('admin.dashboard.quotes.index');
 
-Route::middleware(['auth:sanctum', 'verified'])
-    ->get('/admin/dashboard/quotes/{quote}', [\App\Http\Controllers\Admin\QuoteController::class, 'show'])
-    ->name('admin.dashboard.quotes.show');
+    Route::middleware(['auth:sanctum', 'verified'])
+        ->get('/admin/dashboard/quotes/{quote}', [\App\Http\Controllers\Admin\QuoteController::class, 'show'])
+        ->name('admin.dashboard.quotes.show');
 
-Route::middleware(['auth:sanctum', 'verified'])
-    ->get('/admin/dashboard/document', [\App\Http\Controllers\Admin\DocumentController::class, 'index'])
-    ->name('admin.dashboard.document.index');
+    Route::middleware(['auth:sanctum', 'verified'])
+        ->get('/admin/dashboard/document', [\App\Http\Controllers\Admin\DocumentController::class, 'index'])
+        ->name('admin.dashboard.document.index');
 
-Route::middleware(['auth:sanctum', 'verified'])
-    ->get('/admin/dashboard/document/{item}', [\App\Http\Controllers\Admin\DocumentController::class, 'show'])
-    ->name('admin.dashboard.document');
+    Route::middleware(['auth:sanctum', 'verified'])
+        ->get('/admin/dashboard/document/{item}', [\App\Http\Controllers\Admin\DocumentController::class, 'show'])
+        ->name('admin.dashboard.document');
 
-Route::middleware(['auth:sanctum', 'verified'])
-    ->get('/admin/dashboard/document/{item}/page/{page}', [\App\Http\Controllers\Admin\PageController::class, 'show'])
-    ->name('admin.dashboard.page');
+    Route::middleware(['auth:sanctum', 'verified'])
+        ->get('/admin/dashboard/document/{item}/page/{page}', [\App\Http\Controllers\Admin\PageController::class, 'show'])
+        ->name('admin.dashboard.page');
 
-Route::middleware(['auth:sanctum', 'verified'])
-    ->get('/admin/dashboard', \App\Http\Controllers\Admin\DashboardController::class)
-    ->name('admin.dashboard');
+    Route::middleware(['auth:sanctum', 'verified'])
+        ->get('/admin/dashboard', \App\Http\Controllers\Admin\DashboardController::class)
+        ->name('admin.dashboard');
 
-Route::middleware(['auth:sanctum', 'verified'])
-    ->get('/admin/dashboard/target-publish-dates', \App\Http\Livewire\Admin\TargetPublishDates::class)
-    ->name('admin.dashboard.target-publish-dates.index');
+    Route::middleware(['auth:sanctum', 'verified'])
+        ->get('/admin/dashboard/target-publish-dates', \App\Http\Livewire\Admin\TargetPublishDates::class)
+        ->name('admin.dashboard.target-publish-dates.index');
+});
+
