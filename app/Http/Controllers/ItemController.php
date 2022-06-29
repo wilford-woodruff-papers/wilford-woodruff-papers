b882b57274e8b9de4e96ec99b8b9fe1a7747f8c4
Jon Fackrell <jon.fackrell@gmail.com>
2022-06-29 11:16:21 -0700

    Alternate way of sorting decade and year

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -25,7 +25,6 @@ class ItemController extends Controller
                         ->select('decade', DB::raw('count(*) as total'))
                         ->whereEnabled(1)
                         ->whereNotNull('decade')
-                        ->orderBy('decade', 'ASC')
                         ->groupBy('decade');
         if ($request->has('type') && ($request->get('type') == Type::firstWhere('name', 'Letters')->id)) {
             $decades = $decades->where('type_id', $request->get('type'))
@@ -36,7 +35,6 @@ class ItemController extends Controller
                         ->select('year', DB::raw('count(*) as total'))
                         ->whereEnabled(1)
                         ->whereNotNull('year')
-                        ->orderBy('year', 'ASC')
                         ->groupBy('year');
         if ($request->has('decade')) {
             $years = $years->where('type_id', $request->get('type'))
