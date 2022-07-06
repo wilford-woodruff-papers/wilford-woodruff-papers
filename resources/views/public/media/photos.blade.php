7b46499a70aeb287e571b00ccbbab2ebeb85ee9e
Jon Fackrell <jon.fackrell@gmail.com>
2022-07-06 10:02:28 -0600

    Develop (#247)
    * Fixed typo

--- resources/views/public/media/photos.blade.php
+++ resources/views/public/media/photos.blade.php
@@ -29,7 +29,7 @@
                                             @if($photo->tags->count() > 0)
                                                 <p class="mt-4">
                                                     @foreach($photo->tags as $tag)
-                                                        <a href="{{ route('media.photos', ['tag[]' => $photo->name]) }}"
+                                                        <a href="{{ route('media.photos', ['tag[]' => $tag->name]) }}"
                                                            class="inline-flex items-center px-2 py-0.5 text-xs font-medium bg-secondary text-white">
                                                             <svg xmlns="http://www.w3.org/2000/svg" class="-ml-0.5 mr-2 h-2 w-2 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z" />
