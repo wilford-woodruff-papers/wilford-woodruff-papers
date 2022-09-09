05dd587f07f1c6c159670168ba6d535e544fddc0
Jon Fackrell <jon.fackrell@gmail.com>
2022-09-09 09:56:22 -0600

    Added subject, press, and pages to sitemap

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Item;
+use App\Models\Press;
+use App\Models\Subject;
 use Illuminate\Http\Request;
 
 class SitemapController extends Controller
@@ -12,8 +14,18 @@ class SitemapController extends Controller
         return response()->view('sitemap', [
             'documents' => Item::query()
                             ->select('uuid', 'updated_at')
+                            ->with('pages')
                             ->where('enabled', 1)
                             ->get(),
+            'presses' => Press::query()
+                                ->select('title', 'slug', 'updated_at')
+                                ->orderBy('date', 'DESC')
+                                ->get(),
+            'subjects' => Subject::query()
+                                ->select('name', 'slug', 'updated_at')
+                                ->whereEnabled(1)
+                                ->whereHas('pages')
+                                ->get(),
         ])->withHeaders([
             'Content-Type' => 'application/xml'
         ]);
