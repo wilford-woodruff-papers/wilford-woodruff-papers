8741b64ced61c3a442c1fa3f00cd34ed8b04b6a3
Jon Fackrell <jon.fackrell@gmail.com>
2022-09-28 18:29:09 -0600

    no message

--- app/Imports/JournalsPcfImport.php
+++ app/Imports/JournalsPcfImport.php
@@ -2,6 +2,7 @@
 
 namespace App\Imports;
 
+use App\Models\Item;
 use Carbon\Carbon;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
@@ -18,11 +19,30 @@ class JournalsPcfImport implements ToCollection, WithHeadingRow
         set_time_limit(600);
 
         foreach ($rows as $row) {
-            if (data_get($row, str('Unique Identifier')->lower()->snake()->toString())) {
-                logger()->info(
+            $slug = $this->getSLug(data_get($row, str('URL of Column E')->lower()->snake()->toString()));
+
+            if (! empty($slug)) {
+                $item = Item::query()
+                                ->firstWhere('ftp_slug', $slug);
+                if (! empty($item)) {
+                    $uniqueID = data_get($row, str('Unique Identifier')->lower()->snake()->toString());
+                    $transcriptionCompleted = $this->toCarbonDate(data_get($row, str('Completed Transcriptions Uploaded to FTP')->lower()->snake()->toString()));
+                    $twoLVCompleted = $this->toCarbonDate(data_get($row, str('2LV Completion Date')->lower()->snake()->toString()));
+                    $subjectLinksCompleted = $this->toCarbonDate(data_get($row, str('Subject Links Completed')->lower()->snake()->toString()));
+                    $stylizationCompleted = $this->toCarbonDate(data_get($row, str('Stylization Completed')->lower()->snake()->toString()));
+                    $topicTaggingAssigned = $this->toCarbonDate(data_get($row, str('Date Topic Tagging Assigned')->lower()->snake()->toString()));
+                    $topicTaggingComplete = $this->toCarbonDate(data_get($row, str('Date Topic Tagging Completed')->lower()->snake()->toString()));
+
+                    $item->pcf_unique_id = $uniqueID;
+                    $item->save();
+                } else {
+                    logger()->warning('Could not find item for: '.$slug);
+                }
+
+                /*logger()->info(
                     collect([
                         data_get($row, str('Unique Identifier')->lower()->snake()->toString()),
-                        data_get($row, str('Images Uploaded to FTP')->lower()->snake()->toString()),
+                        $this->getSlug(data_get($row, str('URL of Column E')->lower()->snake()->toString())),
                         $this->toCarbonDate(data_get($row, str('Completed Transcriptions Uploaded to FTP')->lower()->snake()->toString())),
                         $this->toCarbonDate(data_get($row, str('2LV Completion Date')->lower()->snake()->toString())),
                         $this->toCarbonDate(data_get($row, str('Subject Links Completed')->lower()->snake()->toString())),
@@ -31,9 +51,14 @@ class JournalsPcfImport implements ToCollection, WithHeadingRow
                         $this->toCarbonDate(data_get($row, str('Date Topic Tagging Completed')->lower()->snake()->toString())),
                     ])
                     ->join(' | ')
-                );
+                );*/
             }
         }
+    }
+
+    private function getSlug($url)
+    {
+        return str($url)->afterLast('/')->before('?')->toString();
     }
 
     private function toCarbonDate($stringDate)
@@ -44,9 +69,9 @@ class JournalsPcfImport implements ToCollection, WithHeadingRow
 
         try {
             if (is_numeric($stringDate)) {
-                return Carbon::instance(Date::excelToDateTimeObject($stringDate))->toDateString();
+                return Carbon::instance(Date::excelToDateTimeObject($stringDate));
             } else {
-                return Carbon::createFromFormat('d-m-Y', $stringDate)->toDateString();
+                return Carbon::createFromFormat('d-m-Y', $stringDate);
             }
         } catch (\Exception $exception) {
             return $stringDate;
