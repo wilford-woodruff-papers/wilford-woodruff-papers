f5f9fbd45b8eb4d15ee66de0391749bee8f4056c
Jon Fackrell <jon.fackrell@gmail.com>
2022-10-04 11:06:34 -0600

    no message

--- app/Imports/JournalsPcfImport.php
+++ app/Imports/JournalsPcfImport.php
@@ -5,9 +5,13 @@ namespace App\Imports;
 use App\Models\Action;
 use App\Models\ActionType;
 use App\Models\Item;
+use App\Models\User;
 use Carbon\Carbon;
+use Hashids\Math\Bc;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use PhpOffice\PhpSpreadsheet\Shared\Date;
@@ -35,78 +39,92 @@ class JournalsPcfImport implements ToCollection, WithHeadingRow
                         $uniqueID = data_get($row, str('Unique Identifier')->lower()->snake()->toString());
                         $transcriptionCompleted = $this->toCarbonDate(data_get($row, str('Completed Transcriptions Uploaded to FTP')->lower()->snake()->toString()));
                         $twoLVCompleted = $this->toCarbonDate(data_get($row, str('2LV Completion Date')->lower()->snake()->toString()));
+                        $twoLVAssigned = data_get($row, str('2LV Assigned')->lower()->snake()->toString());
                         $subjectLinksCompleted = $this->toCarbonDate(data_get($row, str('Subject Links Completed')->lower()->snake()->toString()));
+                        $subjectLinksAssigned = data_get($row, str('Subject Links Assigned')->lower()->snake()->toString());
                         $stylizationCompleted = $this->toCarbonDate(data_get($row, str('Stylization Completed')->lower()->snake()->toString()));
+                        $stylizationAssigned = data_get($row, str('Stylization Assigned')->lower()->snake()->toString());
                         $topicTaggingAssigned = $this->toCarbonDate(data_get($row, str('Date Topic Tagging Assigned')->lower()->snake()->toString()));
+                        $topicTaggingAssignedTo = data_get($row, str('Topic Tagging Assigned')->lower()->snake()->toString());
                         $topicTaggingComplete = $this->toCarbonDate(data_get($row, str('Date Topic Tagging Completed')->lower()->snake()->toString()));
 
                         $item->pcf_unique_id = $uniqueID;
                         $item->save();
 
-                        Action::firstOrCreate([
-                            'action_type_id' => $actionTypes->firstWhere('name', 'Transcription')->id,
-                            'actionable_type' => Item::class,
-                            'action_id' => $item->id,
-                        ], [
-                            'assigned_to' => null,
-                            'assigned_at' => $transcriptionCompleted,
-                            'completed_by' => null,
-                            'completed_at' => $transcriptionCompleted,
-                            'created_at' => $transcriptionCompleted,
-                            'updated_at' => $transcriptionCompleted,
-                        ]);
+                        if (! empty($transcriptionCompleted)) {
+                            Action::firstOrCreate([
+                                'action_type_id' => $actionTypes->firstWhere('name', 'Transcription')->id,
+                                'actionable_type' => Item::class,
+                                'action_id' => $item->id,
+                            ], [
+                                'assigned_to' => null,
+                                'assigned_at' => $transcriptionCompleted,
+                                'completed_by' => null,
+                                'completed_at' => $transcriptionCompleted,
+                                'created_at' => $transcriptionCompleted,
+                                'updated_at' => $transcriptionCompleted,
+                            ]);
+                        }
 
-                        Action::firstOrCreate([
-                            'action_type_id' => $actionTypes->firstWhere('name', 'Verification')->id,
-                            'actionable_type' => Item::class,
-                            'action_id' => $item->id,
-                        ], [
-                            'assigned_to' => null,
-                            'assigned_at' => $twoLVCompleted,
-                            'completed_by' => null,
-                            'completed_at' => $twoLVCompleted,
-                            'created_at' => $twoLVCompleted,
-                            'updated_at' => $twoLVCompleted,
-                        ]);
+                        if (! empty($twoLVCompleted)) {
+                            Action::firstOrCreate([
+                                'action_type_id' => $actionTypes->firstWhere('name', 'Verification')->id,
+                                'actionable_type' => Item::class,
+                                'action_id' => $item->id,
+                            ], [
+                                'assigned_to' => $this->getUserID($twoLVAssigned),
+                                'assigned_at' => $twoLVCompleted,
+                                'completed_by' => $this->getUserID($twoLVAssigned),
+                                'completed_at' => $twoLVCompleted,
+                                'created_at' => $twoLVCompleted,
+                                'updated_at' => $twoLVCompleted,
+                            ]);
+                        }
 
-                        Action::firstOrCreate([
-                            'action_type_id' => $actionTypes->firstWhere('name', 'Subject Tagging')->id,
-                            'actionable_type' => Item::class,
-                            'action_id' => $item->id,
-                        ], [
-                            'assigned_to' => null,
-                            'assigned_at' => $subjectLinksCompleted,
-                            'completed_by' => null,
-                            'completed_at' => $subjectLinksCompleted,
-                            'created_at' => $subjectLinksCompleted,
-                            'updated_at' => $subjectLinksCompleted,
-                        ]);
+                        if (! empty($subjectLinksCompleted)) {
+                            Action::firstOrCreate([
+                                'action_type_id' => $actionTypes->firstWhere('name', 'Subject Tagging')->id,
+                                'actionable_type' => Item::class,
+                                'action_id' => $item->id,
+                            ], [
+                                'assigned_to' => $this->getUserID($subjectLinksAssigned),
+                                'assigned_at' => $subjectLinksCompleted,
+                                'completed_by' => $this->getUserID($subjectLinksAssigned),
+                                'completed_at' => $subjectLinksCompleted,
+                                'created_at' => $subjectLinksCompleted,
+                                'updated_at' => $subjectLinksCompleted,
+                            ]);
+                        }
 
-                        Action::firstOrCreate([
-                            'action_type_id' => $actionTypes->firstWhere('name', 'Stylization')->id,
-                            'actionable_type' => Item::class,
-                            'action_id' => $item->id,
-                        ], [
-                            'assigned_to' => null,
-                            'assigned_at' => $stylizationCompleted,
-                            'completed_by' => null,
-                            'completed_at' => $stylizationCompleted,
-                            'created_at' => $stylizationCompleted,
-                            'updated_at' => $stylizationCompleted,
-                        ]);
+                        if (! empty($stylizationCompleted)) {
+                            Action::firstOrCreate([
+                                'action_type_id' => $actionTypes->firstWhere('name', 'Stylization')->id,
+                                'actionable_type' => Item::class,
+                                'action_id' => $item->id,
+                            ], [
+                                'assigned_to' => $this->getUserID($stylizationAssigned),
+                                'assigned_at' => $stylizationCompleted,
+                                'completed_by' => $this->getUserID($stylizationAssigned),
+                                'completed_at' => $stylizationCompleted,
+                                'created_at' => $stylizationCompleted,
+                                'updated_at' => $stylizationCompleted,
+                            ]);
+                        }
 
-                        Action::firstOrCreate([
-                            'action_type_id' => $actionTypes->firstWhere('name', 'Topic Tagging')->id,
-                            'actionable_type' => Item::class,
-                            'action_id' => $item->id,
-                        ], [
-                            'assigned_to' => null,
-                            'assigned_at' => $topicTaggingAssigned,
-                            'completed_by' => null,
-                            'completed_at' => $topicTaggingComplete,
-                            'created_at' => $topicTaggingAssigned,
-                            'updated_at' => $topicTaggingComplete,
-                        ]);
+                        if (! empty($topicTaggingAssigned)) {
+                            Action::firstOrCreate([
+                                'action_type_id' => $actionTypes->firstWhere('name', 'Topic Tagging')->id,
+                                'actionable_type' => Item::class,
+                                'action_id' => $item->id,
+                            ], [
+                                'assigned_to' => $this->getUserID($topicTaggingAssignedTo),
+                                'assigned_at' => $topicTaggingAssigned,
+                                'completed_by' => $this->getUserID($topicTaggingAssignedTo),
+                                'completed_at' => $topicTaggingComplete,
+                                'created_at' => $topicTaggingAssigned,
+                                'updated_at' => $topicTaggingComplete,
+                            ]);
+                        }
                     });
                 } else {
                     logger()->warning('Could not find item for: '.$slug);
@@ -149,5 +167,153 @@ class JournalsPcfImport implements ToCollection, WithHeadingRow
         } catch (\Exception $exception) {
             return $stringDate;
         }
+    }
+
+    private function getUserID($initials)
+    {
+        switch ($initials) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        SJ
+        ELH
+        BD
+        AA
+        NH
+        CC
+        SW
+        OC
+        KL
+            case 'SCH':
+                $name = 'Steve Harper';
+                $email = str($name)->lower()->replace(' ', '.').'@wilfordwoodruffpapers.org';
+                break;
+            case 'TH':
+                $name = 'Thomas Harper';
+                $email = str($name)->lower()->replace(' ', '.').'@wilfordwoodruffpapers.org';
+                break;
+            case 'AD':
+            case 'AP':
+                $name = 'Ashlyn Pells';
+                $email = str($name)->lower()->replace(' ', '.').'@wilfordwoodruffpapers.org';
+                break;
+            case 'KT':
+                $name = 'Kristy Taylor';
+                $email = str($name)->lower()->replace(' ', '.').'@wilfordwoodruffpapers.org';
+                break;
+            case 'JM':
+                $name = 'Jennifer Mackley';
+                $email = str($name)->lower()->replace(' ', '.').'@wilfordwoodruffpapers.org';
+                break;
+            case 'EM':
+                $name = 'Elise Mackley';
+                $email = str($name)->lower()->replace(' ', '.').'@wilfordwoodruffpapers.org';
+                break;
+            case 'Eli M':
+                $name = 'Eli Mackley';
+                $email = str($name)->lower()->replace(' ', '.').'@wilfordwoodruffpapers.org';
+                break;
+            case 'Lorin':
+            case 'LG':
+                $name = 'Lorin Groesbeck';
+                $email = str($name)->lower()->replace(' ', '.').'@wilfordwoodruffpapers.org';
+                break;
+            case 'EE':
+                $name = 'Elaine Esposito';
+                $email = str($name)->lower()->replace(' ', '.').'@wilfordwoodruffpapers.org';
+                break;
+            case 'EH':
+                $name = 'Emma Hadfield';
+                $email = str($name)->lower()->replace(' ', '.').'@wilfordwoodruffpapers.org';
+                break;
+            case 'SF':
+                $name = 'Sherilyn Farnes';
+                $email = str($name)->lower()->replace(' ', '.').'@wilfordwoodruffpapers.org';
+                break;
+            case 'JG':
+                $name = 'Jason Godfrey';
+                $email = str($name)->lower()->replace(' ', '.').'@wilfordwoodruffpapers.org';
+                break;
+            case 'HK':
+                $name = 'Hailee Kotter';
+                $email = str($name)->lower()->replace(' ', '.').'@wilfordwoodruffpapers.org';
+                break;
+            case 'RM':
+                $name = 'Rebecca Matos';
+                $email = str($name)->lower()->replace(' ', '.').'@wilfordwoodruffpapers.org';
+                break;
+            case 'MS':
+                $name = 'Marinda Smith';
+                $email = str($name)->lower()->replace(' ', '.').'@wilfordwoodruffpapers.org';
+                break;
+            case 'AH':
+                $name = 'Abigail Harper';
+                $email = str($name)->lower()->replace(' ', '.').'@wilfordwoodruffpapers.org';
+                break;
+            case 'DG':
+                $name = 'Darby Glass';
+                $email = str($name)->lower()->replace(' ', '.').'@wilfordwoodruffpapers.org';
+                break;
+            case 'SJ':
+                $name = 'Sarah Jorgensen';
+                $email = str($name)->lower()->replace(' ', '.').'@wilfordwoodruffpapers.org';
+                break;
+            case 'ELH':
+                $name = 'Ellie Hancock';
+                $email = str($name)->lower()->replace(' ', '.').'@wilfordwoodruffpapers.org';
+                break;
+            case 'BD':
+                $name = 'Braeden Dyer';
+                $email = str($name)->lower()->replace(' ', '.').'@wilfordwoodruffpapers.org';
+                break;
+            case 'AA':
+                $name = 'Allison Andrews';
+                $email = str($name)->lower()->replace(' ', '.').'@wilfordwoodruffpapers.org';
+                break;
+            case 'NH':
+                $name = 'Natalie Hancock';
+                $email = str($name)->lower()->replace(' ', '.').'@wilfordwoodruffpapers.org';
+                break;
+            case 'CC':
+                $name = 'Cory Clay';
+                $email = str($name)->lower()->replace(' ', '.').'@wilfordwoodruffpapers.org';
+                break;
+            case 'SW':
+                $name = 'Samuel Webb';
+                $email = str($name)->lower()->replace(' ', '.').'@wilfordwoodruffpapers.org';
+                break;
+            case 'OC':
+                $name = 'Oliver Carson';
+                $email = str($name)->lower()->replace(' ', '.').'@wilfordwoodruffpapers.org';
+                break;
+            case 'KL':
+                $name = 'Karly Lay';
+                $email = str($name)->lower()->replace(' ', '.').'@wilfordwoodruffpapers.org';
+                break;
+            default:
+                $name = 'Jon Fackrell';
+                $email = str($name)->lower()->replace(' ', '.').'@wilfordwoodruffpapers.org';
+            }
+        $user = User::firstOrCreate([
+            'email' => $email,
+        ], [
+            'name' => $name,
+            'password' => Hash::make(Str::uuid()),
+        ]);
+
+        return $user->id;
     }
 }
