4913c983a6fcb7238afb1be622e4d2da257150fa
Jon Fackrell <jon.fackrell@gmail.com>
2022-10-20 16:08:06 -0600

    removed unused import

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
