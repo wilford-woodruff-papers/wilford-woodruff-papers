8605386446a58e24e8f1b1bf30284f7c15c4acc7
Jon Fackrell <jon.fackrell@gmail.com>
2022-10-21 12:57:13 -0600

    show all children when logged in

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -35,9 +35,11 @@ class Subject extends Model
     public function children()
     {
         return $this->hasMany(self::class)->with(['children' => function ($query) {
-            $query->when(auth()->guest() || (auth()->check() && ! auth()->user()->hasAnyRole(['Super Admin'])), fn ($query) => $query->where('hide_on_index', 0))
-                ->where('tagged_count', '>', 0)
-                ->orWhere('text_count', '>', 0);
+            $query->when(auth()->guest() || (auth()->check() && ! auth()->user()->hasAnyRole(['Super Admin'])), fn ($query) => $query->where('hide_on_index', 0)
+                ->where(function ($query) {
+                    $query->where('tagged_count', '>', 0)
+                        ->orWhere('text_count', '>', 0);
+                }));
         }]);
     }
 
