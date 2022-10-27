07bbf88e095a2b9efd6548550fad3359f5bfa9cd
Jon Fackrell <jon.fackrell@gmail.com>
2022-10-27 15:13:57 -0600

    Store instagram files on spaces to prevent the timeouts

--- app/Console/Commands/ImportInstagramFeedCommand.php
+++ app/Console/Commands/ImportInstagramFeedCommand.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Models\SocialMedia;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
 
 class ImportInstagramFeedCommand extends Command
 {
@@ -15,31 +16,60 @@ class ImportInstagramFeedCommand extends Command
     public function handle()
     {
         $profile = \Dymantic\InstagramFeed\Profile::for('wilford_woodruff_papers');
-        $feed = $profile?->refreshFeed(10);
+        $feed = $profile?->refreshFeed(2);
 
-        foreach($feed as $post){
-            if($post->type == 'video'){
+        foreach ($feed as $post) {
+            if ($post->type == 'video') {
                 continue;
             }
 
             $socialMedia = SocialMedia::updateOrCreate([
-                'social_id' => $post->id
+                'social_id' => $post->id,
             ], [
-                'title' => 'Instagram: ' . $post->id,
+                'title' => 'Instagram: '.$post->id,
                 'social_type' => $post->type,
-                'cover_image' => ($post->type == 'video' ? $post->thumbnail_url : $post->url),
-                'embed' => ($post->type == 'video' ? $post->url : null),
+                'cover_image' => ($post->type == 'video' ? $this->storeMediaToLocalDisk($post->thumbnail_url) : $this->storeMediaToLocalDisk($post->url)),
+                'embed' => ($post->type == 'video' ? $this->storeMediaToLocalDisk($post->url) : null),
                 'description' => $post->caption,
                 'excerpt' => str($post->caption)->before('#'),
                 'link' => $post->permalink,
                 'date' => Carbon::createFromFormat(\DateTime::ISO8601, $post->timestamp),
             ]);
-            if($post->isCarousel()){
+            if ($post->isCarousel()) {
                 /*dd($post->children);*/
-                $socialMedia->extra_attributes = $post->children;
+                $socialMedia->extra_attributes = $this->storeMedias($post->children);
                 $socialMedia->save();
             }
         }
+    }
 
+    private function storeMedias($images)
+    {
+        foreach ($images as &$image) {
+            $image['url'] = $this->storeMediaToLocalDisk($image['url'], $image['id']);
+        }
+
+        return $images;
+    }
+
+    private function storeMediaToLocalDisk($image, $name = null)
+    {
+        $extension = str($image)->before('?')->afterLast('.');
+
+        if (empty($name)) {
+            $name = str($image)->before('?')->afterLast('/')->before('.');
+        }
+
+        $path = 'instagram/'.$name.'.'.$extension;
+
+        if (Storage::disk('instagram')->exists($path)) {
+            return Storage::disk('instagram')->url($path);
+        }
+
+        $contents = file_get_contents($image);
+
+        if (Storage::disk('instagram')->put($path, $contents)) {
+            return Storage::disk('instagram')->url($path);
+        }
     }
 }
--- config/filesystems.php
+++ config/filesystems.php
@@ -105,6 +105,18 @@ return [
             'visibility' => 'public',
         ],
 
+        'instagram' => [
+            'driver' => 's3',
+            'key' => env('DO_SPACES_KEY'),
+            'secret' => env('DO_SPACES_SECRET'),
+            'region' => env('DO_SPACES_REGION'),
+            'bucket' => env('DO_SPACES_BUCKET'),
+            'url' => env('DO_SPACES_URL'),
+            'endpoint' => env('DO_SPACES_ENDPOINT'),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'visibility' => 'public',
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
