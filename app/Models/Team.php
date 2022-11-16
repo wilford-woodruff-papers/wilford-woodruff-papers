70eb260b6e5727d22a17a039d2b3d707994bf3c5
Jon Fackrell <jon.fackrell@gmail.com>
2022-11-16 10:40:29 -0700

    Allow people and teams to be sorted in Nova

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -4,12 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\EloquentSortable\Sortable;
+use Spatie\EloquentSortable\SortableTrait;
 
-class Team extends Model
+class Team extends Model implements Sortable
 {
     use HasFactory;
+    use SortableTrait;
 
     protected $guarded = ['id'];
+
+    public $sortable = [
+        'order_column_name' => 'order',
+        'sort_when_creating' => true,
+    ];
 
     public function boardmembers()
     {
