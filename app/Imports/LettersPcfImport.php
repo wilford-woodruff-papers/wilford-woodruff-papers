efaf09d13a50f073b28e23de2024897b22ad4304
Jon Fackrell <jon.fackrell@gmail.com>
2023-01-02 23:12:27 -0700

    Develop (#294)
    * linting
    
    * started book page
    
    * Attempt to add Prettier for tailwind class sorting
    
    * no message
    
    * add spacing between pagination
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * Finished overall layout and improved mobile design
    
    * no message
    
    * Last changes to book product page
    
    * updated laravel and tailwind
    
    * no message
    
    * final changes to new design
    
    * no message
    
    * Giving Tuesday
    
    Added a giving Tuesday page which can really just be a new donation page
    
    * Added button hover colors and inlined countdown timer on mobile
    
    * Updated Autobiographies PCF import
    
    * Added letters PCF to enable letters
    
    * Added activity report
    
    * Remove HTMLparser as dependency for parsing subjects in transcripts
    
    * Add Letters PCF importer
    
    * Rename Letters Enabler
    
    * WIP
    
    * WIP
    
    * Upload updates
    
    * Query optimization
    
    * WIP
    
    * WIP
    
    * Put letter processing in on queue
    
    * Remove loging entire row
    
    * WIP
    
    * Loop document types rather than hard code
    
    * WIP
    
    * Added additional users and queing

--- app/Imports/LettersPcfImport.php
+++ app/Imports/LettersPcfImport.php
@@ -3,18 +3,19 @@
 namespace App\Imports;
 
 use App\Jobs\ImportLetterFromPcf;
-use Maatwebsite\Excel\Concerns\ToModel;
-use Maatwebsite\Excel\Concerns\WithBatchInserts;
-use Maatwebsite\Excel\Concerns\WithChunkReading;
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class LettersPcfImport implements ToModel, WithBatchInserts, WithChunkReading, WithHeadingRow
+class LettersPcfImport implements ToCollection, WithHeadingRow
 {
     public $id;
 
-    public function model(array $row)
+    public function collection(Collection $rows)
     {
-        ImportLetterFromPcf::dispatch($row);
+        foreach ($rows as $row) {
+            ImportLetterFromPcf::dispatch($row);
+        }
     }
 
     public function chunkSize(): int
