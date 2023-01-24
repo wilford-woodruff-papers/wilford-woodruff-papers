d201628837847f8715f1c0012d4f5f25b49bbd40
Jon Fackrell <jon.fackrell@gmail.com>
2023-01-23 17:18:18 -0700

    Add reconciliation report

---
+++ app/Imports/FTPChecker.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\Item;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Storage;
+use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+
+class FTPChecker implements ToCollection, WithHeadingRow
+{
+    public function collection(Collection $rows)
+    {
+        if (Storage::fileExists(storage_path('app/public/reconcile_ftp.csv'))) {
+            Storage::delete(storage_path('app/public/reconcile_ftp.csv'));
+        }
+
+        $filename = storage_path('app/public/reconcile_ftp.csv');
+        $handle = fopen($filename, 'w');
+        fputcsv($handle, [
+            'Type',
+            'Name',
+            'Website URL',
+            'FTP URL',
+        ]);
+
+        foreach ($rows as $row) {
+            $transcribedPages = $row['pages_needing_review'] + $row['pages_marked_blank'] + $row['pages_transcribed'];
+            $totalPages = $row['total_pages'];
+
+            if ($transcribedPages !== $totalPages) {
+                continue;
+            }
+
+            $document = Item::query()
+                ->with(['type'])
+                ->whereNotNull('ftp_id')
+                ->whereDoesntHave('actions', function (Builder $query) {
+                    $query->where('action_type_id', 1);
+                })
+                ->where('ftp_id', 'https://fromthepage.com/iiif/'.$row['fromthepage_id'].'/manifest')
+                ->first();
+
+            if (! empty($document)) {
+                fputcsv($handle, [
+                    $document->type?->name,
+                    $document->name,
+                    route('admin.dashboard.document', ['item' => $document->uuid]),
+                    'https://fromthepage.com/woodruff/woodruffpapers/'.$document->ftp_slug,
+                ]);
+            }
+        }
+    }
+}
---
+++ app/Nova/Actions/ImportFtpMetadataExport.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Nova\Actions;
+
+use App\Imports\FTPChecker;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Collection;
+use Laravel\Nova\Actions\Action;
+use Laravel\Nova\Fields\ActionFields;
+use Laravel\Nova\Fields\File;
+use Maatwebsite\Excel\Facades\Excel;
+
+class ImportFtpMetadataExport extends Action
+{
+    use InteractsWithQueue, Queueable, SerializesModels;
+
+    public $standalone = true;
+
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Perform the action on the given models.
+     *
+     * @param  \Laravel\Nova\Fields\ActionFields  $fields
+     * @param  \Illuminate\Support\Collection  $models
+     * @return mixed
+     */
+    public function handle(ActionFields $fields, Collection $models)
+    {
+        Excel::import(new FTPChecker(), $fields->file);
+
+        return Action::message('Imported successfully');
+    }
+
+    /**
+     * Get the fields available on the action.
+     *
+     * @return array
+     */
+    public function fields()
+    {
+        return [
+            File::make('File')
+                ->rules('required'),
+        ];
+    }
+}
