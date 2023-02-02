60121c4e38342abb406d32fb8fc42b9995815e86
Jon Fackrell <jon.fackrell@gmail.com>
2023-02-02 12:31:46 -0700

    Added contest exports

---
+++ app/Nova/Actions/ExportContestEntries.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Nova\Actions;
+
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\LaravelNovaExcel\Actions\DownloadExcel;
+
+class ExportContestEntries extends DownloadExcel implements WithMapping, WithHeadings
+{
+    /**
+     * @return array
+     */
+    public function headings(): array
+    {
+        return [
+            'Internal ID',
+            'Title',
+            'Division',
+            'Category',
+            'Medium',
+            'Connection',
+            'Link',
+            'File URL',
+            'Date',
+        ];
+    }
+
+    /**
+     * @param $item
+     * @return array
+     */
+    public function map($entry): array
+    {
+        $entry->load('contestants');
+
+        return [
+            $entry->id,
+            $entry->title,
+            $entry->division,
+            $entry->category,
+            $entry->medium,
+            $entry->connection,
+            $entry->link,
+            $entry->getFirstMediaUrl(),
+            $entry->created_at->toDateTimeString(),
+        ];
+    }
+}
