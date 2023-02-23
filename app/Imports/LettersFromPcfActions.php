509e54840eafe8ab9fb40abec6a8f7df14ec1c61
Jon Fackrell <jon.fackrell@gmail.com>
2023-02-23 10:45:04 -0700

    Develop (#306)
    * WIP
    
    * Upload updates
    
    * Query optimization
    
    * WIP
    
    * WIP
    
    * Put letter processing in on queue
    
    * Remove loging entire row
    
    * WIP
    
    * Loop document types rather than hard code
    
    * WIP
    
    * WIP
    
    * Added additional users and queing
    
    * Updating partners
    
    * Update to partners page
    
    * Fix release actions
    
    * added more formatting an layout
    
    * Added file manager
    
    * Revert "Added file manager"
    
    This reverts commit 308cde645d129c7de57d12cceb8d7a4c352f47e3.
    
    * Require Identifier
    
    * Revert "Require Identifier"
    
    This reverts commit 304268a05c7b04cdc6e9948a68d0f75b7ffaa470.
    
    * Adding template to item fields and new PCF action
    
    * Updating models and schema
    
    * Automatically add transcription task when importing from FTP
    
    * Added item editing form
    
    * Added sticky action buttons to form and success message
    
    * Adding template mangement to Nova
    
    * Added new document form with type (template selection)
    
    * Auto calculate Unique ID on create
    
    * Added single action button to mark Actions as done
    
    * Added spatie horizon watcher package instead
    
    * Add collection for attached media
    
    * Delete existing pages
    
    * Formating
    
    * Mostly style changes
    
    * Added discourse and additional documents master file import
    
    * Add people and places import
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * Sort months in timeline
    
    * Undo changes
    
    * Hide new button temporarily
    
    * Colorizing
    
    * When auto publishing, add/remove publish task
    
    * Added letter reimporter to fix some of the original import issues

--- app/Imports/LettersFromPcfActions.php
+++ app/Imports/LettersFromPcfActions.php
@@ -3,8 +3,9 @@
 namespace App\Imports;
 
 use App\Jobs\ImportLettersMasterFileAction;
-use App\Jobs\ImportNewLettersFromPcfAction;
+use App\Jobs\UpdateLetterFromPcfAction;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Bus;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
@@ -19,16 +20,27 @@ class LettersFromPcfActions implements ToCollection, WithHeadingRow
 
     public function collection(Collection $rows)
     {
+        $jobs = [];
+
         foreach ($rows as $row) {
             switch ($this->action) {
                 case 'Import New':
-                    ImportNewLettersFromPcfAction::dispatch($row);
+                    if (! empty(data_get($row, 'transcription_completion_date'))) {
+                        $jobs[] = new UpdateLetterFromPcfAction($row);
+                    }
+                    //ImportNewLettersFromPcfAction::dispatch($row);
                     break;
                 case 'Import Master File':
                     ImportLettersMasterFileAction::dispatch($row);
                     break;
             }
         }
+
+        $batch = Bus::batch($jobs)
+            ->onQueue('import')
+            ->name($this->action)
+            ->allowFailures()
+            ->dispatch();
     }
 
     public function chunkSize(): int
--- config/logging.php
+++ config/logging.php
@@ -60,6 +60,12 @@ return [
             'level' => env('LOG_LEVEL', 'debug'),
         ],
 
+        'imports' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/imports.log'),
+            'level' => env('LOG_LEVEL', 'debug'),
+        ],
+
         'daily' => [
             'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),
