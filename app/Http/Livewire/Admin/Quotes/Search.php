c97196821dc08a564e935d5b20ecd1b4af9bcad5
Jon Fackrell <jon.fackrell@gmail.com>
2023-03-02 14:55:21 -0700

    Added quote searching to admin

---
+++ app/Http/Livewire/Admin/Quotes/Search.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Quotes;
+
+use Livewire\Component;
+
+class Search extends Component
+{
+    public $readyToLoad = false;
+
+    public $selectedTopic = null;
+
+    public $search = null;
+
+    public $topics = [];
+
+    protected $queryString = [
+        'selectedTopic',
+        'search',
+    ];
+
+    public function render()
+    {
+        $quotes = \App\Models\Quote::query()
+            ->with([
+                'page',
+                'continuation',
+                'topics',
+            ])
+            ->whereHas('actions')
+            ->whereNull('continued_from_previous_page')
+            ->when($this->selectedTopic, function ($query) {
+                $query->whereHas('topics', function ($query) {
+                    $query->where('subjects.id', $this->selectedTopic);
+                });
+            })
+            ->when($this->search, function ($query) {
+                $query->where('text', 'LIKE', '%'.$this->search.'%');
+            });
+
+        if ($this->readyToLoad && empty($this->topics)) {
+            $this->topics = \App\Models\Subject::query()
+                ->select([
+                    'subjects.id',
+                    'subjects.name',
+                ])
+                ->withCount('pages')
+                ->whereHas('category', function ($query) {
+                    $query->where('name', 'Topics');
+                })
+                ->whereNull('subject_id')
+                ->has('pages')
+                ->orderBy('name')
+                ->get()
+                ->toArray();
+        }
+
+        return view('livewire.admin.quotes.search', [
+            'quotes' => $quotes->paginate(20),
+        ])
+            ->layout('layouts.admin');
+    }
+
+    public function clearTopic()
+    {
+        $this->selectedTopic = null;
+    }
+
+    public function load()
+    {
+        $this->readyToLoad = true;
+    }
+}
