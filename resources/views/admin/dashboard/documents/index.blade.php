348a20018415a9587a70a953282e7d166c5f1ca7
Jon Fackrell <jon.fackrell@gmail.com>
2023-03-09 11:50:21 -0700

    Develop (#314)
    * Remove loging entire row
    
    * WIP
    
    * Loop document types rather than hard code
    
    * WIP
    
    * WIP
    
    * Added additional users and queing
    
    * Updating partners
    
    * Update to partners page
    
    * Fix release actions
    
    * added more formatting an layout
    
    * Added file manager
    
    * Revert "Added file manager"
    
    This reverts commit 308cde645d129c7de57d12cceb8d7a4c352f47e3.
    
    * Require Identifier
    
    * Revert "Require Identifier"
    
    This reverts commit 304268a05c7b04cdc6e9948a68d0f75b7ffaa470.
    
    * Adding template to item fields and new PCF action
    
    * Updating models and schema
    
    * Automatically add transcription task when importing from FTP
    
    * Added item editing form
    
    * Added sticky action buttons to form and success message
    
    * Adding template mangement to Nova
    
    * Added new document form with type (template selection)
    
    * Auto calculate Unique ID on create
    
    * Added single action button to mark Actions as done
    
    * Added spatie horizon watcher package instead
    
    * Add collection for attached media
    
    * Delete existing pages
    
    * Formating
    
    * Mostly style changes
    
    * Added discourse and additional documents master file import
    
    * Add people and places import
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * Sort months in timeline
    
    * Undo changes
    
    * Hide new button temporarily
    
    * Colorizing
    
    * When auto publishing, add/remove publish task
    
    * Added letter reimporter to fix some of the original import issues
    
    * Added page activity view
    
    * View Journal Section in Dashboard
    
    * Updated progress matrix
    
    * Added progress graphic numbers
    
    * Made approved changes to progress matrix
    
    * Update UI parser
    
    * allow suffix to be 0-2 characters
    
    * Add first date job to assign a date to all pages
    
    * Added quote searching to admin
    
    * Reformatted documents page
    
    * WIP
    
    * WIP
    
    * WIP

--- resources/views/admin/dashboard/documents/index.blade.php
+++ resources/views/admin/dashboard/documents/index.blade.php
@@ -1,11 +1,3 @@
 <x-admin-layout>
-    <div class="mx-auto mt-4 max-w-7xl">
-        <div class="py-4">
-
-            <div class="flex-col space-y-4">
-                <livewire:admin.documents.dashboard />
-            </div>
-
-        </div>
-    </div>
+    <livewire:admin.documents.dashboard />
 </x-admin-layout>
--- resources/views/components/admin/quotes/table.blade.php
+++ resources/views/components/admin/quotes/table.blade.php
@@ -1,18 +1,20 @@
 <!-- Activity table (small breakpoint and up) -->
-<div class="overflow-hidden overflow-x-auto min-w-full align-middle shadow sm:rounded-lg">
+<div {{ $attributes->merge(['class' => 'min-w-full align-middle shadow sm:rounded-lg']) }}>
     <table
         x-data="{
                 open: $persist(null)
             }"
         class="min-w-full divide-y divide-cool-gray-200">
-        <thead>
-        <tr>
-            {{ $head }}
-        </tr>
+        <thead class="sticky top-0 z-50"
+               :class="shadow && 'drop-shadow-md'"
+        >
+            <tr class="bg-gray-100">
+                {{ $head }}
+            </tr>
         </thead>
 
         <tbody class="bg-white divide-y divide-cool-gray-200">
-        {{ $body }}
+            {{ $body }}
         </tbody>
     </table>
 </div>
