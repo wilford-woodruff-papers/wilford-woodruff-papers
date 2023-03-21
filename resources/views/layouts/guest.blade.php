ce7bf47bf6850b07bd4fa19ad7f1ca10fff19a65
Jon Fackrell <jon.fackrell@gmail.com>
2023-03-21 11:50:51 -0600

    Check for not set title

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -8,7 +8,7 @@
 
         <meta property="og:site_name" content="Wilford Woodruff Papers">
 
-        <x-open-graph-image::metatags title="{{ str($title)->before('-')->trim() ?? null }}" />
+        <x-open-graph-image::metatags title="{{ str($title ?? null)->before('-')->trim() }}" />
 
         <meta property="og:description" content="Explore Wilford Woodruff's powerful eyewitness account of the Restoration">
         <meta property="og:type" content="website" />
