8dcb21759019b850b7a82e0c252d83d018c5ea2c
Jon Fackrell <jon.fackrell@gmail.com>
2023-03-21 15:22:57 -0600

    Added current URL rather than base url

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -13,7 +13,7 @@
         <meta property="og:description" content="Explore Wilford Woodruff's powerful eyewitness account of the Restoration">
         <meta property="og:type" content="website" />
         <meta property="og:image" content="https://wilfordwoodruffpapers.org/img/wilford-woodruff.png">
-        <meta property="og:url" content="https://wilfordwoodruffpapers.org/">
+        <meta property="og:url" content="{{ request()->url() }}">
         <meta name="twitter:card" content="summary_large_image">
         <meta name="description" content="Explore Wilford Woodruff's powerful eyewitness account of the Restoration"/>
         <meta name="keywords" content="Wilford Woodruff, Restoration, Prophet, The Church of Jesus Christ of Latter-day Saints"/>
