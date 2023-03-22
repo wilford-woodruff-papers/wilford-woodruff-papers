e8a7077ece086448ed4b4e50f1411eb3040671f8
Jon Fackrell <jon.fackrell@gmail.com>
2023-03-22 08:49:18 -0600

    Add Facebook sharing button to quotes search page

--- resources/views/livewire/admin/quotes/search.blade.php
+++ resources/views/livewire/admin/quotes/search.blade.php
@@ -1,5 +1,10 @@
 <div wire:init="load">
-    <div class="grid grid-cols-12 gap-x-4">
+    <div x-data="{
+            share: function(url){
+                window.open(url, 'Share', 'width=560,height=640');
+            }
+        }"
+        class="grid grid-cols-12 gap-x-4">
         <div class="col-span-2">
             <div wire:loading
                  wire:target="load"
@@ -93,6 +98,14 @@
                                        class="text-secondary"
                                     >
                                         {{ $quote->page->parent?->name }}
+                                    </a>
+                                </div>
+                                <div class="p-2">
+                                    <a x-on:click.prevent="share('https://facebook.com/sharer.php?u={{ route('short-url.page', ['hashid' => $quote->page->hashid]) }}')"
+                                        id="share-to-facebook"
+                                       class="p-1 text-white bg-secondary hover:bg-secondary-dark"
+                                       href="https://facebook.com/sharer.php?u={{ route('short-url.page', ['hashid' => $quote->page->hashid]) }}">
+                                        <svg class="inline w-8 h-8" aria-label="Share on Facebook" xmlns="http://www.w3.org/2000/svg" fill="white" stroke="none" viewBox="0 0 24 24"><path d="M13.397,20.997v-8.196h2.765l0.411-3.209h-3.176V7.548c0-0.926,0.258-1.56,1.587-1.56h1.684V3.127 C15.849,3.039,15.025,2.997,14.201,3c-2.444,0-4.122,1.492-4.122,4.231v2.355H7.332v3.209h2.753v8.202H13.397z"/></svg>
                                     </a>
                                 </div>
                             </td>
