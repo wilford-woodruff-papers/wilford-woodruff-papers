19d7a302e35e6e42e802b056a58c9c2d2cbc7c9b
Jon Fackrell <jon.fackrell@gmail.com>
2023-04-20 21:26:38 -0600

    WIP

--- resources/views/public/conference/summary-page.blade.php
+++ resources/views/public/conference/summary-page.blade.php
@@ -26,7 +26,36 @@
             Featured Speakers
         </h2>
 
-        <div class="grid grid-cols-1 px-16 md:grid-cols-3 md:h-[580px]">
+        <div class="grid grid-cols-1 gap-8 px-8">
+            @foreach($speakers as $speaker)
+                <div class="grid grid-cols-5">
+                    <div class="col-span-2">
+                        <iframe class="w-full aspect-[16/9]"
+                                src="{{ $speaker['video'] }}?rel=0"
+                                title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    </div>
+                    <div class="col-span-3">
+                        <div class="py-4 text-center">
+                            <div class="text-2xl font-black text-dark-blue">
+                                {{ $speaker['name'] }}
+                            </div>
+                            @if(! empty($speaker['title']))
+                                <div class="text-2xl text-dark-blue">
+                                    {{ $speaker['title'] }}
+                                </div>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+                @if(! empty($speaker['bio']))
+                    <div class="text-base text-dark-blue">
+                        {!! $speaker['bio'] !!}
+                    </div>
+                @endif
+            @endforeach
+        </div>
+{{--
+        <div class="grid z-50 grid-cols-1 px-16 md:grid-cols-3 md:h-[580px]">
             <div class="grid order-2 content-end h-full md:order-1">
                 <img src="{{ asset('img/conferences/2023/Jennifer-Mackley.png') }}"
                      alt=""
@@ -36,9 +65,17 @@
                     <div class="text-2xl font-black text-dark-blue">
                         Jennifer Ann Mackley
                     </div>
-                    <div class="text-2xl text-dark-blue">
-
-                    </div>
+                    <a href="https://drive.google.com/file/d/1JSWddDy2GAp6viIfOhnawWSN2LmyDDmS/view?usp=share_link"
+                       target="_blank"
+                       class="flex justify-center items-center text-2xl text-dark-blue">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z" />
+                        </svg>
+                        <span class="ml-2">
+                            Watch
+                        </span>
+                    </a>
                 </div>
             </div>
             <div class="grid order-1 content-start h-full md:order-2">
@@ -71,7 +108,7 @@
             </div>
         </div>
 
-        <div class="grid grid-cols-1 px-16 md:grid-cols-3 md:-mt-24 md:h-[580px]">
+        <div class="grid z-40 grid-cols-1 px-16 md:grid-cols-3 md:-mt-24 md:h-[580px]">
             <div class="grid order-1 content-end h-full md:order-1">
                 <img src="{{ asset('img/conferences/2023/Amy-Harris.png') }}"
                      alt=""
@@ -87,7 +124,7 @@
                 </div>
             </div>
             <div class="grid order-2 content-start h-full md:order-2">
-                {{--<img src="{{ asset('img/conferences/2023/Steve-Harper.png') }}"
+                --}}{{--<img src="{{ asset('img/conferences/2023/Steve-Harper.png') }}"
                      alt=""
                      class="w-full h-auto"
                 />
@@ -98,7 +135,7 @@
                     <div class="text-2xl text-dark-blue">
                         Keynote Speaker
                     </div>
-                </div>--}}
+                </div>--}}{{--
             </div>
             <div class="grid order-3 content-end h-full md:order-3">
                 <img src="{{ asset('img/conferences/2023/Steven-Wheelright.png') }}"
@@ -115,7 +152,8 @@
                 </div>
             </div>
         </div>
-        <div class="grid grid-cols-1 px-16 md:grid-cols-3 md:-mt-24 md:h-[580px]">
+
+        <div class="grid z-30 grid-cols-1 px-16 md:grid-cols-3 md:-mt-24 md:h-[580px]">
             <div class="grid order-1 content-end h-full md:order-1">
                 <img src="{{ asset('img/conferences/2023/Hovan-Lawton.png') }}"
                      alt=""
@@ -159,6 +197,7 @@
                 </div>
             </div>
         </div>
+        --}}
     </div>
 
     <div class="h-16"></div>
@@ -167,5 +206,76 @@
         <x-gallery.photo />
     </div>
 
+    <div class="mx-auto max-w-7xl">
+        {{--<div class="grid grid-cols-4 gap-4">
+            @foreach($photos as $photo)
+                <div class="overflow-hidden aspect-[16/9]">
+                    <img src="{{ \Illuminate\Support\Facades\Storage::disk('spaces')->url($photo) }}"
+                         class="w-full h-auto"
+                         alt=""/>
+                </div>
+            @endforeach
+        </div>--}}
+        <div class="gallery-container" id="animated-thumbnails-gallery">
+            @foreach($photos as $photo)
+                <a href="{{ \Illuminate\Support\Facades\Storage::disk('spaces')->url(str($photo)->replace('thumbnails', 'medium')) }}" data-lg-size="1600-2400">
+                    <img alt=""
+                         src="{{ \Illuminate\Support\Facades\Storage::disk('spaces')->url($photo) }}"
+                    />
+                </a>
+            @endforeach
+        </div>
+    </div>
+    @push('scripts')
+        <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/justifiedGallery@3.8.1/dist/css/justifiedGallery.css'>
+        <link type="text/css" rel="stylesheet" href="{{ asset('css/lightgallery.css') }}" />
 
+        <!-- lightgallery plugins -->
+        <link type="text/css" rel="stylesheet" href="{{ asset('css/lg-zoom.css') }}" />
+        <link type="text/css" rel="stylesheet" href="{{ asset('css/lg-thumbnail.css') }}" />
+
+        <script src='https://cdn.jsdelivr.net/npm/justifiedGallery@3.8.1/dist/js/jquery.justifiedGallery.js'></script>
+        <script src="{{ asset('js/lightgallery.min.js') }}"></script>
+
+        <!-- lightgallery plugins -->
+        <script src="{{ asset('js/light-ballery-plugins/lg-thumbnail.min.js') }}"></script>
+        <script src="{{ asset('js/light-ballery-plugins/lg-zoom.min.js') }}"></script>
+        <script type="text/javascript">
+
+            jQuery("#animated-thumbnails-gallery")
+                .justifiedGallery({
+                    captions: false,
+                    lastRow: "hide",
+                    rowHeight: 180,
+                    margins: 5
+                })
+                .on("jg.complete", function () {
+                    window.lightGallery(
+                        document.getElementById("animated-thumbnails-gallery"),
+                        {
+                            autoplayFirstVideo: false,
+                            pager: false,
+                            galleryId: "nature",
+                            mousewheel: true,
+                            plugins: [lgZoom, lgThumbnail],
+                            mobileSettings: {
+                                controls: false,
+                                showCloseIcon: false,
+                                download: false,
+                                rotate: false
+                            }
+                        }
+                    );
+                });
+            /*lightGallery(document.getElementById('animated-thumbnails-gallery'), {
+                plugins: [lgZoom, lgThumbnail],
+                animateThumb: false,
+                zoomFromOrigin: false,
+                allowMediaOverlap: true,
+                toggleThumb: true,
+                speed: 500,
+                licenseKey: '0000-0000-000-0000'
+            });*/
+        </script>
+    @endpush
 </x-guest-layout>
