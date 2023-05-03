3bf632e2af9be1781fec0eb6586faa466578f147
Jon Fackrell <jon.fackrell@gmail.com>
2023-05-03 16:57:57 -0600

    Add user roles dropdown in admin profile dropdown menu

--- resources/views/components/admin/profile-dropdown.blade.php
+++ resources/views/components/admin/profile-dropdown.blade.php
@@ -25,12 +25,24 @@
     -->
     <div x-show="open"
          x-cloak
-         class="absolute right-0 py-1 mt-2 w-48 bg-white rounded-md ring-1 ring-black ring-opacity-5 shadow-lg origin-top-right focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+         class="absolute right-0 py-1 mt-2 w-48 bg-white rounded-md divide-y divide-gray-100 ring-1 ring-black ring-opacity-5 shadow-lg origin-top-right focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
         <!-- Active: "bg-gray-100", Not Active: "" -->
-        <a href="#" class="block py-2 px-4 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
+        <div class="py-1" role="none">
+            <a href="#" class="block py-2 px-4 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
 
-        <a href="#" class="block py-2 px-4 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
+            <a href="#" class="block py-2 px-4 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
 
-        <a href="#" class="block py-2 px-4 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
+            <a href="#" class="block py-2 px-4 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
+        </div>
+        <div class="py-1" role="none">
+            <span class="block py-2 px-4 text-sm text-gray-900 uppercase" role="menuitem" tabindex="-1" id="user-roles-item-0">Roles</span>
+            <div class="overflow-y-scroll max-h-96">
+                @foreach(auth()->user()->roles->sortBy('name') as $key => $role)
+                    <span class="block py-1 pr-4 pl-8 text-sm text-gray-600" role="menuitem" tabindex="-1" id="user-roles-item-{{ $key }}">
+                    {{ $role->name }}
+                </span>
+                @endforeach
+            </div>
+        </div>
     </div>
 </div>
