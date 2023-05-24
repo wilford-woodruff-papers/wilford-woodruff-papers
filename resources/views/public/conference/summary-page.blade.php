54996d9c756e9078e67ac180ca4dc6585df885ab
Jon Fackrell <jon.fackrell@gmail.com>
2023-05-24 17:00:21 -0600

    Adjusted article link

--- resources/views/public/conference/summary-page.blade.php
+++ resources/views/public/conference/summary-page.blade.php
@@ -61,8 +61,8 @@
                                class="group"
                                 target="_blank"
                             >
-                                <div class="flex gap-x-6 justify-center items-center text-lg text-secondary">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="inline mr-2 w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <div class="flex gap-x-2 justify-center items-center pt-4 text-lg text-secondary">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="inline w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>
                                     </svg>
                                     <span class="group-hover:underline">
