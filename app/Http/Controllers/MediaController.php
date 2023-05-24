cf1d29b7bd44bfa74d844d03538e11c0d3eaf58b
Jon Fackrell <jon.fackrell@gmail.com>
2023-05-24 15:38:48 -0600

    Refactor media and article preview carousel queries to include filtering by date

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -67,6 +67,7 @@ class MediaController extends Controller
     {
         return view('public.media.videos', [
             'videos' => Video::latest('date')
+                                ->whereDate('date', '<=', DB::raw('NOW()'))
                                 ->orderBy('title', 'ASC')
                                 ->when($request->has('tag'), function ($query) use ($request) {
                                     $query->withAnyTags($request->get('tag'), 'videos');
