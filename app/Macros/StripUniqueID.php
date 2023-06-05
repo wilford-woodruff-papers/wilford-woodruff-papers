b980677e6e32cde1f46736229517596ef013c3cb
Jon Fackrell <jon.fackrell@gmail.com>
2023-06-05 16:48:52 -0600

    Add StripUniqueID macro to Stringable class

---
+++ app/Macros/StripUniqueID.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Macros;
+
+class StripUniqueID
+{
+    public function __invoke()
+    {
+        return function () {
+            return new static(str($this->value)->replaceMatches('/\[.*?\]/', '')->trim());
+        };
+    }
+}
