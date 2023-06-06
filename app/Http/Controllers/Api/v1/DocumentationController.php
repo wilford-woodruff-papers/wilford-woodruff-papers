5ad6a989f65872a4ac9af0f102f8fcf5f1573f3d
Jon Fackrell <jon.fackrell@gmail.com>
2023-06-06 15:41:39 -0600

    Add token count check to DocumentationController

--- app/Http/Controllers/Api/v1/DocumentationController.php
+++ app/Http/Controllers/Api/v1/DocumentationController.php
@@ -8,6 +8,10 @@ class DocumentationController extends Controller
 {
     public function __invoke()
     {
+        if (auth()->user()->tokens()->count() < 1) {
+            return redirect()->route('api-tokens.index');
+        }
+
         return view('api.v1.documentation');
     }
 }
