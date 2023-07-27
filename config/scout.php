d8c5c6453a2990953d88867949575aa30ec11974
Jon Fackrell <jon.fackrell@gmail.com>
2023-07-26 23:19:21 -0600

    added date to sortable attributes

--- config/scout.php
+++ config/scout.php
@@ -148,6 +148,7 @@ return [
                 ],
                 'sortableAttributes' => [
                     'name',
+                    'date',
                 ],
                 'typoTolerance' => [
                     'minWordSizeForTypos' => [
