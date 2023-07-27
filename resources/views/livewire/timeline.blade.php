9a1c4fc43e9b57fa054712b1718236b4de3eb2aa
Jon Fackrell <jon.fackrell@gmail.com>
2023-07-27 14:41:46 -0600

    Updated styles and added column borders

--- resources/views/livewire/timeline.blade.php
+++ resources/views/livewire/timeline.blade.php
@@ -15,16 +15,16 @@
          x-init="$watch('activeEvent', (value, oldValue) => $wire.set('event', value))"
          class="grid grid-cols-5">
         <div class="relative col-span-1 bg-gray-200">
-            <div class="sticky top-10">
+            <div class="sticky top-0">
                 Left Sidebar
             </div>
         </div>
-        <div class="col-span-3 pb-96 bg-gray-400">
+        <div class="col-span-3 pb-96 bg-gray-100">
             Main
             <div x-ref="container" class="relative pt-32">
 
-                <div class="sticky top-32 z-10 bg-white">
-                    <div class="grid grid-cols-6 items-center text-center">
+                <div class="sticky top-24 z-10">
+                    <div class="grid z-50 grid-cols-6 items-center text-center bg-white">
                         <div></div>
                         @foreach($groups as $group)
                             <div x-on:click="{{ str($group)->snake() }} = ! {{ str($group)->snake() }}"
@@ -34,13 +34,17 @@
                         @endforeach
                     </div>
                     <div id="event-selector"
-                         class="top-10 w-full h-20 bg-gray-800 opacity-25">
+                         class="top-0 w-full h-20 bg-gray-800 opacity-25">
 
                     </div>
                 </div>
                 <div class="">
+                    <div class="flex sticky top-0 z-10 justify-center items-center my-8 w-full h-24 text-4xl font-bold text-white bg-primary">
+                        1800
+                    </div>
                     @php
                         $previousYear = null;
+                        //$currentYear = 1800;
                     @endphp
                     @foreach ($hits as $key => $events)
                         @php
@@ -49,16 +53,43 @@
                         @endphp
                         @if(($previousYear < $currentYear))
                             @if(($currentYear % 10) == 0)
-                                <div class="flex sticky top-10 justify-center items-center my-8 w-full h-24 text-4xl font-bold text-white bg-primary">
+                                <div class="grid grid-cols-6 px-4 h-8 divide-x divide-slate-200">
+                                    <div>&nbsp;</div>
+                                    <div>&nbsp;</div>
+                                    <div>&nbsp;</div>
+                                    <div>&nbsp;</div>
+                                    <div>&nbsp;</div>
+                                    <div>&nbsp;</div>
+                                </div>
+
+                                <div class="flex sticky top-0 z-10 justify-center items-center w-full h-24 text-4xl font-bold text-white bg-primary">
                                     {{ $currentYear }}
                                 </div>
+
+                                <div class="grid grid-cols-6 px-4 h-8 divide-x divide-slate-200">
+                                    <div>&nbsp;</div>
+                                    <div>&nbsp;</div>
+                                    <div>&nbsp;</div>
+                                    <div>&nbsp;</div>
+                                    <div>&nbsp;</div>
+                                    <div>&nbsp;</div>
+                                </div>
                             @else
-                                <div class="flex items-center my-2 w-full text-2xl font-bold text-gray-900">
-                                    {{ $currentYear }}
+                                <div class="grid grid-cols-6 px-4 h-14 divide-x divide-slate-200">
+                                    <div>
+                                        <p class="text-2xl font-bold text-gray-900">
+                                            {{ $currentYear }}
+                                        </p>
+                                    </div>
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
                                 </div>
                             @endif
                         @endif
-                        <div class="grid grid-cols-6 px-4 mt-4">
+                        <div class="grid grid-cols-6 px-4 h-14 divide-x divide-slate-200">
                             <div>
                                 {{ \Carbon\Carbon::createFromTimestamp($key)->toFormattedDateString() }}
                             </div>
@@ -72,7 +103,7 @@
                                 <div x-show="{{ str($group)->snake() }}"
                                      @class([
                                         str($group)->slug().' col-span-1',
-                                        'border-t-2' => $count == 0,
+                                        'border-t-0' => $count == 0,
                                       ])
                                      class="{{ str($group)->slug() }} col-span-1"
                                 >
@@ -87,8 +118,8 @@
                                             >
                                                 <div @class([
                                                         'absolute w-80 text-sm font-normal bg-white hover:z-10',
-                                                        'left-0' => ($key <= 2),
-                                                        'right-0' => ($key > 2),
+                                                        'left-2' => ($key <= 2),
+                                                        'right-2' => ($key > 2),
                                                     ])>
                                                     <div class="flex">
                                                         <div @class([
@@ -135,7 +166,7 @@
         </div>
         <div class="relative col-span-1 bg-gray-200">
 
-            <div class="sticky top-10 py-8 px-4 h-screen bg-primary">
+            <div class="sticky top-0 py-8 px-4 h-screen bg-primary">
                 Right Sidebar
                 {{--@if($event)
                     @php
