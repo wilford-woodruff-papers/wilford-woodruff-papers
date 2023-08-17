f63a1e5152a8859189a28e15e38f8c721e8ab4f6
Jon Fackrell <jon.fackrell@gmail.com>
2023-08-17 16:32:30 -0600

    Added newsletter subscription to registration

--- app/Http/Controllers/Auth/AcceptTermsOfUseController.php
+++ app/Http/Controllers/Auth/AcceptTermsOfUseController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Actions\SubscribeToConstantContactAction;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -27,11 +28,23 @@ class AcceptTermsOfUseController extends Controller
     /**
      * Process form.
      */
-    public function submit(Request $request): RedirectResponse
+    public function submit(Request $request, SubscribeToConstantContactAction $subscribeToConstantContactAction): RedirectResponse
     {
         $user = Auth::user();
         $user->accepted_terms = 1;
         $user->save();
+
+        if ($request->boolean('subscribe_to_newsletter') == true) {
+            $subscribeToConstantContactAction->execute([
+                'email' => $user->email,
+                'first_name' => $user->first_name,
+                'last_name' => $user->last_name,
+            ],
+                [
+                    config('wwp.list_memberships.newsletter'),
+                ]
+            );
+        }
 
         $home = session('url.intended') ?? '/';
 
