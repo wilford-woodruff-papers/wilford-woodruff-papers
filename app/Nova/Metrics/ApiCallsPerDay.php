dbaa5b752abd1d92ae8f51d34c0d44f28bc773dd
Jon Fackrell <jon.fackrell@gmail.com>
2023-08-23 14:59:07 -0600

    We needed a way to view api calls

---
+++ app/Nova/Metrics/ApiCallsPerDay.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Nova\Metrics;
+
+use Laravel\Nova\Http\Requests\NovaRequest;
+use Laravel\Nova\Metrics\Trend;
+use Spatie\Activitylog\Models\Activity;
+
+class ApiCallsPerDay extends Trend
+{
+    /**
+     * Calculate the value of the metric.
+     *
+     * @return mixed
+     */
+    public function calculate(NovaRequest $request)
+    {
+        return $this->countByDays(
+            $request,
+            Activity::where('log_name', 'api')
+                ->where('event', 'accessed')
+        )
+            ->showSumValue();
+    }
+
+    /**
+     * Get the ranges available for the metric.
+     *
+     * @return array
+     */
+    public function ranges()
+    {
+        return [
+            30 => __('30 Days'),
+            60 => __('60 Days'),
+            90 => __('90 Days'),
+        ];
+    }
+
+    /**
+     * Determine the amount of time the results of the metric should be cached.
+     *
+     * @return \DateTimeInterface|\DateInterval|float|int|null
+     */
+    public function cacheFor()
+    {
+        return now()->addHours(2);
+    }
+
+    /**
+     * Get the URI key for the metric.
+     *
+     * @return string
+     */
+    public function uriKey()
+    {
+        return 'api-calls-per-day';
+    }
+}
