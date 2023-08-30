a37b426fe1fd934951fcbc35ea9274dc542da214
Jon Fackrell <jon.fackrell@gmail.com>
2023-08-30 17:04:46 -0600

    added media library disk

--- config/filesystems.php
+++ config/filesystems.php
@@ -111,6 +111,19 @@ return [
             'visibility' => 'public',
         ],
 
+        'media_library' => [
+            'driver' => 's3',
+            'key' => env('DO_SPACES_KEY'),
+            'secret' => env('DO_SPACES_SECRET'),
+            'region' => env('DO_SPACES_REGION'),
+            'bucket' => env('DO_SPACES_BUCKET'),
+            'root' => 'media-library',
+            'url' => env('DO_SPACES_URL'),
+            'endpoint' => env('DO_SPACES_ENDPOINT'),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'visibility' => 'public',
+        ],
+
         'authors' => [
             'driver' => 's3',
             'key' => env('DO_SPACES_KEY'),
