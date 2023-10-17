3f11d8807ec1e55ea606d7700a35ec8ab9e7987a
Jon Fackrell <jon.fackrell@gmail.com>
2023-10-17 10:14:24 -0600

    Added userstamps to items and creator and editor to Nova

---
+++ database/migrations/2023_10_17_160710_add_userstamps_to_items_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('items', function (Blueprint $table) {
+            $table->after('updated_at', function (Blueprint $table) {
+                $table->unsignedBigInteger('created_by')
+                    ->nullable();
+                $table->unsignedBigInteger('updated_by')
+                    ->nullable();
+            });
+        });
+    }
+};
