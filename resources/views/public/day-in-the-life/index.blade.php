d3a57a38cb0186575bab5a832c4df6ec9103bcbe
Jon Fackrell <jon.fackrell@gmail.com>
2023-11-03 12:17:54 -0600

    open places and people in new window

--- resources/views/public/day-in-the-life/index.blade.php
+++ resources/views/public/day-in-the-life/index.blade.php
@@ -38,7 +38,7 @@
             </div>
 
             <div class="relative bg-white">
-                <ul role="list" class="flex gap-6 items-center">
+                <ul role="list" class="flex flex-col gap-6 md:flex-row md:items-center">
                     @if($pages->count() > 0)
                         <li class="flex-1 bg-white divide-y divide-gray-200 shadow">
                             <a href="#documents">
@@ -57,7 +57,6 @@
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
                                         </svg>
-
                                     </div>
                                 </div>
                             </a>
@@ -274,7 +273,7 @@
                                                     </div>
                                                 </div>
                                                 <div class="">
-                                                    <div class="text-lg line-clamp-8">
+                                                    <div class="text-lg line-clamp-6">
                                                         {!!
                                                             str($page->transcript)
                                                                 ->extractContentOnDate($date)
@@ -303,7 +302,7 @@
                             People
                         </h2>
                     </div>
-                    <div class="grid grid-cols-3 gap-2">
+                    <div class="grid grid-cols-1 gap-2 lg:grid-cols-3">
                         @foreach($people as $person)
                             <div class="p-4 border border-gray-300 shadow-lg">
                                 <div>
@@ -362,7 +361,7 @@
                             Places
                         </h2>
                     </div>
-                    <div class="grid grid-cols-3">
+                    <div class="grid grid-cols-1 gap-y-4 lg:grid-cols-3">
                         @foreach($places as $place)
                             <div class="">
                                 <a href="{{ route('subjects.show', ['subject' => $place->slug]) }}"
@@ -405,7 +404,7 @@
                                                 .addTo(this.map)
                                                 .bindPopup(`
                                                     <a href="{{ route('subjects.show', ['subject' => $place->slug]) }}"
-                                                       class="text-base !text-secondary popup"
+                                                       class="text-base !text-secondary"
                                                        target="_blank"
                                                     >
                                                         {{ $place->name }}
