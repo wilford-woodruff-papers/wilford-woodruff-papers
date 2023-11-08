b16924db696cbfb987f1157f070db6c76f0a3ccb
Jon Fackrell <jon.fackrell@gmail.com>
2023-11-08 13:36:56 -0700

    decreased queries required to show subject

--- resources/views/components/quote-summary.blade.php
+++ resources/views/components/quote-summary.blade.php
@@ -1,6 +1,6 @@
 <li class="grid grid-cols-7 py-4">
     <div class="col-span-1 px-2">
-        <a class="col-span-1 my-2 mx-auto w-20 h-auto" href="{{ route('pages.show', ['item' => $quote->page->item, 'page' => $quote->page]) }}">
+        <a class="col-span-1 my-2 mx-auto w-20 h-auto" href="{{ route('pages.show', ['item' => $quote->page->parent, 'page' => $quote->page]) }}">
             <img src="{{ $quote->page->getFirstMedia()?->getUrl('thumb') }}"
                  alt=""
                  loading="lazy"
@@ -9,20 +9,20 @@
     </div>
     <div class="col-span-6 py-2 px-4">
         <p class="pb-1 text-lg font-medium capitalize text-secondary">
-            <a href="{{ route('pages.show', ['item' => $quote->page->item, 'page' => $quote->page]) }}">Page {{ $quote->page->order }}</a>
+            <a href="{{ route('pages.show', ['item' => $quote->page->parent, 'page' => $quote->page]) }}">Page {{ $quote->page->order }}</a>
         </p>
         <div class="flex gap-x-3 ml-2 text-base font-medium">
             <div>
                 <span class="text-gray-600">Part of </span>
                 <span class="text-secondary">
-                    <a href="{{ route('documents.show', ['item' => $quote->page->item]) }}">{{ \Illuminate\Support\Str::of($quote->page->item?->name)->replaceMatches('/\[.*?\]/', '')->trim() }}</a>
+                    <a href="{{ route('documents.show', ['item' => $quote->page->parent]) }}">{{ \Illuminate\Support\Str::of($quote->page->parent?->name)->replaceMatches('/\[.*?\]/', '')->trim() }}</a>
                 </span>
             </div>
             <div>
                 @auth()
                     @hasanyrole('CFM Researcher')
                     <div>
-                        @if($quote->page->item?->enabled)
+                        @if($quote->page->parent?->enabled)
                             <span class="inline-flex items-center py-0.5 px-2.5 text-xs font-medium text-green-800 bg-green-100 rounded-full">
                                     {{ __('Published') }}
                                 </span>
@@ -36,8 +36,8 @@
                 @endauth
             </div>
         </div>
-        {{--@if($page->item->type)
-            <p class="ml-2 text-base text-primary">{{ $page->item->type->name }}</p>
+        {{--@if($page->parent->type)
+            <p class="ml-2 text-base text-primary">{{ $page->parent->type->name }}</p>
         @endif--}}
         <div class="py-2 px-4 font-serif text-sm text-gray-500">
             @if(! empty($quote->text))
@@ -80,7 +80,7 @@
         {{--@hasanyrole('Editor|Admin|Super Admin')
             <div class="ml-3">
                 <p class="text-sm text-gray-900">Real name: {{ $page->name }}</p>
-                <p class="text-sm text-gray-900">Item: {{ $page->item->name }}</p>
+                <p class="text-sm text-gray-900">Item: {{ $page->parent->name }}</p>
                 @if($page->quotes_count > 0)
                     <p class="text-sm text-gray-900">Quotes: {{ $page->quotes_count }}</p>
                 @endif
