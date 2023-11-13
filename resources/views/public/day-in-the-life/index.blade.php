9c79013de5ad5f804c29252009ed937733168930
Jon Fackrell <jon.fackrell@gmail.com>
2023-11-13 15:59:35 -0700

    added background to journal entry

--- resources/views/public/day-in-the-life/index.blade.php
+++ resources/views/public/day-in-the-life/index.blade.php
@@ -6,82 +6,91 @@
                     :text="'Day in the Life'"
     />
     <div x-data="dayInTheLife"
-         @scroll.window="$refs.nav.getBoundingClientRect().top < 10 ? scrolledFromTop = true : scrolledFromTop = false"
-         class="py-12 px-12 mx-auto max-w-7xl">
-
-        <div class="">
+         @scroll.window="$refs.nav.getBoundingClientRect().top < 10 ? scrolledFromTop = true : scrolledFromTop = false">
+        <div class="py-12">
             <div class="">
-                <div class="flex flex-col gap-y-12">
+                <div class="">
+                    <div class="flex flex-col gap-y-12">
 
-                    @include('public.day-in-the-life.sections.date-navigation', ['location' => 'top'])
+                        <div class="px-12 mx-auto max-w-7xl">
+                            @include('public.day-in-the-life.sections.date-navigation', ['location' => 'top'])
+                        </div>
 
-                    <div class="top-0 z-50 bg-white md:sticky">
-                        <div x-ref="nav">
-                            <ul role="list"
-                                class="flex flex-col transition-all duration-200 md:flex-row md:items-center"
-                                :class="{'gap-6': !scrolledFromTop, 'md:shadow-2xl md:divide-x md:divide-gray-200': scrolledFromTop}"
-                            >
-                                <li class="px-6 pt-1 mx-auto h-full bg-white flex-0"
-                                    :class="{'hidden': !scrolledFromTop, '': scrolledFromTop}"
-                                    x-cloak
+
+                        <div class="top-0 z-50 bg-white md:sticky"
+                             :class="{'': !scrolledFromTop, 'md:shadow-2xl': scrolledFromTop}">
+                            <div x-ref="nav"
+                                 class="px-12 mx-auto max-w-7xl">
+                                <ul role="list"
+                                    class="flex flex-col transition-all duration-200 md:flex-row md:items-center"
+                                    :class="{'gap-6': !scrolledFromTop, 'md:divide-x md:divide-gray-200': scrolledFromTop}"
                                 >
-                                    <div class="h-0">
-                                        <input class="w-0 h-0 border-0" x-ref="nav_picker" type="text"/>
-                                    </div>
-                                    <button class="mx-auto text-gray-900" x-on:click="pickers['nav'].toggle()">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
-                                        </svg>
+                                    <li class="px-6 pt-1 mx-auto h-full bg-white flex-0"
+                                        :class="{'hidden': !scrolledFromTop, '': scrolledFromTop}"
+                                        x-cloak
+                                    >
+                                        <div class="h-0">
+                                            <input class="w-0 h-0 border-0" x-ref="nav_picker" type="text"/>
+                                        </div>
+                                        <button class="mx-auto text-gray-900" x-on:click="pickers['nav'].toggle()">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
+                                            </svg>
 
-                                    </button>
-                                </li>
+                                        </button>
+                                    </li>
 
-                                @foreach($sections as $key => $stat)
-                                    @if(! empty($stat['items']) && $stat['items']->count() > 0)
-                                        <li class="flex-1 bg-white divide-y divide-gray-200 hover:bg-gray-100"
-                                            :class="{'shadow border border-gray-200': !scrolledFromTop, '': scrolledFromTop}">
-                                            <a href="#{{ str($stat['name'])->slug() }}">
-                                                <div class="flex justify-between items-center py-2 px-6 space-x-6 w-full">
-                                                    <div class="flex flex-row flex-1 gap-y-1 gap-x-4 justify-start items-center md:flex-col lg:items-start truncate">
-                                                        <div class="flex items-center space-x-3">
-                                                            <h3 class="text-2xl font-semibold text-gray-900 truncate">
-                                                                {{ $stat['items']->count() }}
-                                                            </h3>
+                                    @foreach($sections as $key => $stat)
+                                        @if(! empty($stat['items']) && $stat['items']->count() > 0)
+                                            <li class="flex-1 bg-white divide-y divide-gray-200 hover:bg-gray-100"
+                                                :class="{'shadow border border-gray-200': !scrolledFromTop, '': scrolledFromTop}">
+                                                <a href="#{{ str($stat['name'])->slug() }}">
+                                                    <div class="flex justify-between items-center py-2 px-6 space-x-6 w-full">
+                                                        <div class="flex flex-row flex-1 gap-y-1 gap-x-4 justify-start items-center md:flex-col lg:items-start truncate">
+                                                            <div class="flex items-center space-x-3">
+                                                                <h3 class="text-2xl font-semibold text-gray-900 truncate">
+                                                                    {{ $stat['items']->count() }}
+                                                                </h3>
+                                                            </div>
+                                                            <p class="mt-1 text-lg font-semibold uppercase text-secondary truncate">
+                                                                {{ str($key)->plural($stat['items']->count()) }}
+                                                            </p>
                                                         </div>
-                                                        <p class="mt-1 text-lg font-semibold uppercase text-secondary truncate">
-                                                            {{ str($key)->plural($stat['items']->count()) }}
-                                                        </p>
+                                                        <div class="hidden flex-shrink-0 justify-center items-center w-10 h-10 lg:flex bg-secondary">
+                                                            <x-dynamic-component :component="$stat['icon']" class="w-6 h-6 text-white"/>
+                                                        </div>
                                                     </div>
-                                                    <div class="hidden flex-shrink-0 justify-center items-center w-10 h-10 lg:flex bg-secondary">
-                                                        <x-dynamic-component :component="$stat['icon']" class="w-6 h-6 text-white"/>
-                                                    </div>
-                                                </div>
-                                            </a>
-                                        </li>
-                                    @endif
-                                @endforeach
-                            </ul>
+                                                </a>
+                                            </li>
+                                        @endif
+                                    @endforeach
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="bg-primary-50">
+                            @include('public.day-in-the-life.sections.journal')
+                        </div>
+
+                        <div class="flex flex-col gap-y-12 px-12 mx-auto max-w-7xl">
+                            @include($sections['Person']['view'], ['section' => $sections['Person']])
+
+                            @include($sections['Place']['view'], ['section' => $sections['Place']])
+
+                            @include($sections['Event']['view'], ['section' => $sections['Event']])
+
+                            @include($sections['Quote']['view'], ['section' => $sections['Quote']])
+
+                            @include($sections['Document']['view'], ['section' => $sections['Document']])
+
+                            @include('public.day-in-the-life.sections.date-navigation', ['location' => 'bottom'])
                         </div>
                     </div>
-
-                    @include('public.day-in-the-life.sections.journal')
-
-                    @include($sections['Documents']['view'], ['section' => $sections['Documents']])
-
-                    @include($sections['People']['view'], ['section' => $sections['People']])
-
-                    @include($sections['Places']['view'], ['section' => $sections['Places']])
-
-                    @include($sections['Events']['view'], ['section' => $sections['Events']])
-
-                    @include($sections['Quotes']['view'], ['section' => $sections['Quotes']])
-
-                    @include('public.day-in-the-life.sections.date-navigation', ['location' => 'bottom'])
-
                 </div>
             </div>
         </div>
     </div>
+
 
     @push('styles')
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
--- resources/views/public/day-in-the-life/sections/journal.blade.php
+++ resources/views/public/day-in-the-life/sections/journal.blade.php
@@ -1,49 +1,56 @@
-<h2 class="text-4xl font-thin uppercase border-b-4 border-highlight">
-    Journal Entry
-</h2>
-<div class="grid grid-cols-1 gap-y-4 gap-x-12 md:grid-cols-3">
-    <div class="order-2 md:order-1 md:col-span-2">
-        <div class="pb-6 text-xl font-thin text-gray-900 md:text-3xl">
-            {{ $date->format('F d, Y ~ l') }}
-        </div>
-        <div class="text-lg leading-relaxed text-gray-900 md:text-2xl">
-            {!! $content !!}
-        </div>
-        @if(! empty($topics))
-            <div class="col-span-3 mt-6">
-                <ul class="flex flex-wrap gap-3">
-                    @foreach($topics as $topic)
-                        <li class="">
-                            <a href="{{ route('subjects.show', ['subject' => $topic->slug]) }}"
-                               class="inline-flex items-center py-1 px-3 text-base text-white bg-secondary"
-                               target="_blank"
-                            >
-                                <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 -ml-0.5 w-4 h-4 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z"></path>
-                                </svg>
-                                {{ $topic->name }}
-                            </a>
-                        </li>
-                    @endforeach
-                </ul>
-
+<div class="px-12 pt-12 pb-14 mx-auto max-w-7xl">
+    <h2 class="text-5xl font-thin text-white uppercase">
+        Journal Entry
+    </h2>
+    <div class="grid grid-cols-1 gap-y-4 bg-white md:grid-cols-3">
+        <div class="order-2 p-4 md:order-1 md:col-span-2">
+            <div class="pb-6 text-xl font-thin text-gray-900 md:text-3xl">
+                {{ $date->format('F d, Y ~ l') }}
             </div>
-        @endif
-    </div>
-    <div class="flex flex-col order-1 gap-y-4 md:order-2">
-        <div class="overflow-hidden">
-            <img src="{{ $day->first()->getfirstMediaUrl(conversionName: 'web') }}"
-                 alt=""
-                 class="w-full h-auto scale-125 cursor-pointer"
-                 x-on:click="Livewire.emit('openModal', 'page', {'pageId': {{ $day->first()->id }}})"
-            />
+            <div class="text-lg leading-relaxed text-gray-900 md:text-2xl">
+                {!! $content !!}
+            </div>
+            @if(! empty($topics))
+                <div class="col-span-3 mt-6">
+                    <ul class="flex flex-wrap gap-3">
+                        @foreach($topics as $topic)
+                            <li class="">
+                                <a href="{{ route('subjects.show', ['subject' => $topic->slug]) }}"
+                                   class="inline-flex items-center py-1 px-3 text-base text-white bg-secondary"
+                                   target="_blank"
+                                >
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 -ml-0.5 w-4 h-4 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z"></path>
+                                    </svg>
+                                    {{ $topic->name }}
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
         </div>
-        <div class="flex justify-center">
-            <button x-on:click="Livewire.emit('openModal', 'page', {'pageId': {{ $day->first()->id }}})">
-                                    <span class="text-secondary">
-                                        View Fullscreen
-                                    </span>
-            </button>
+        <div class="order-1 gap-y-4 md:order-2">
+            <div class="hidden w-full h-full bg-left bg-cover cursor-pointer md:block bg-scale-125"
+                style="background-image: url('{{ $day->first()->getfirstMediaUrl(conversionName: 'web') }}')"
+                 x-on:click="Livewire.emit('openModal', 'page', {'pageId': {{ $day->first()->id }}})"
+            >
+            </div>
+            <div class="block overflow-hidden w-full h-80 shadow-2xl md:hidden">
+                <div class="w-full h-full scale-125"
+                     style="background-image: url('{{ $day->first()->getfirstMediaUrl(conversionName: 'web') }}')"
+                >
+                </div>
+            </div>
+        </div>
+        <div class="order-last pb-1 md:col-span-3">
+            <div class="flex justify-center">
+                <button x-on:click="Livewire.emit('openModal', 'page', {'pageId': {{ $day->first()->id }}})">
+                        <span class="text-lg text-secondary">
+                            View Fullscreen
+                        </span>
+                </button>
+            </div>
         </div>
     </div>
 </div>
