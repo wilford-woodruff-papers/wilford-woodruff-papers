ad5281f9299e38d4f35951d3977123ed1664826a
Jon Fackrell <jon.fackrell@gmail.com>
2023-12-04 22:53:08 -0700

    added familysearch socialite package

---
+++ app/Http/Controllers/Auth/FamilySearchLoginController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
+use Laravel\Socialite\Facades\Socialite;
+
+class FamilySearchLoginController extends Controller
+{
+    /**
+     * Redirect the user to the Google authentication page.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function redirectToProvider()
+    {
+        return Socialite::driver('familysearch')->redirect();
+    }
+
+    /**
+     * Obtain the user information from Google.
+     */
+    public function handleProviderCallback(): RedirectResponse
+    {
+        // $familysearchUser = Socialite::driver('familysearch')->user();
+        $familysearchUser = Socialite::driver('familysearch')->user();
+        dd($familysearchUser);
+    }
+}
