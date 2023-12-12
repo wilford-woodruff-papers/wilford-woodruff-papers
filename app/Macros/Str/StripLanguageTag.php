bb201cfe9c3f25dd221ad17219ee55bff1a3139b
Jon Fackrell <jon.fackrell@gmail.com>
2023-12-12 08:46:36 -0700

    wip

--- app/Macros/Str/StripLanguageTag.php
+++ app/Macros/Str/StripLanguageTag.php
@@ -9,7 +9,58 @@ class StripLanguageTag
     public function __invoke()
     {
         return function ($subject) {
-            return Str::of($subject)->replaceMatches('/\[Language:.*?\][\s|\n]*/', '')->trim();
+            return $this->replaceInlineLanguageTags(
+                $this->removePageLanguageTag($subject)
+            );
         };
+    }
+
+    private function removePageLanguageTag($subject)
+    {
+        return Str::of($subject)
+            ->replaceMatches('/\[Language:.*?\][\s|\n]*/', '')
+            ->trim();
+    }
+
+    private function replaceInlineLanguageTags($subject)
+    {
+        return Str::of($subject)
+            ->replaceMatches('/(?:{)(.*?)(?:})/m', function (array $match) {
+                $parts = str($match[1])->explode('|');
+                switch ($parts->count()) {
+                    case 1:
+                        return `
+                            <div
+                                x-data="{
+                                    tooltipVisible: false,
+                                    tooltipText: 'Shorthand',
+                                    tooltipArrow: true,
+                                    tooltipPosition: 'top',
+                                }"
+                                x-init="$refs.content.addEventListener('mouseenter', () => { tooltipVisible = true; }); $refs.content.addEventListener('mouseleave', () => { tooltipVisible = false; });"
+                                class="relative">
+
+                                <div x-ref="tooltip" x-show="tooltipVisible" :class="{ 'top-0 left-1/2 -translate-x-1/2 -mt-0.5 -translate-y-full' : tooltipPosition == 'top', 'top-1/2 -translate-y-1/2 -ml-0.5 left-0 -translate-x-full' : tooltipPosition == 'left', 'bottom-0 left-1/2 -translate-x-1/2 -mb-0.5 translate-y-full' : tooltipPosition == 'bottom', 'top-1/2 -translate-y-1/2 -mr-0.5 right-0 translate-x-full' : tooltipPosition == 'right' }" class="absolute w-auto text-sm" x-cloak>
+                                    <div x-show="tooltipVisible" x-transition class="relative px-2 py-1 text-white bg-black rounded bg-opacity-90">
+                                        <p x-text="tooltipText" class="flex-shrink-0 block text-xs whitespace-nowrap"></p>
+                                        <div x-ref="tooltipArrow" x-show="tooltipArrow" :class="{ 'bottom-0 -translate-x-1/2 left-1/2 w-2.5 translate-y-full' : tooltipPosition == 'top', 'right-0 -translate-y-1/2 top-1/2 h-2.5 -mt-px translate-x-full' : tooltipPosition == 'left', 'top-0 -translate-x-1/2 left-1/2 w-2.5 -translate-y-full' : tooltipPosition == 'bottom', 'left-0 -translate-y-1/2 top-1/2 h-2.5 -mt-px -translate-x-full' : tooltipPosition == 'right' }" class="absolute inline-flex items-center justify-center overflow-hidden">
+                                            <div :class="{ 'origin-top-left -rotate-45' : tooltipPosition == 'top', 'origin-top-left rotate-45' : tooltipPosition == 'left', 'origin-bottom-left rotate-45' : tooltipPosition == 'bottom', 'origin-top-right -rotate-45' : tooltipPosition == 'right' }" class="w-1.5 h-1.5 transform bg-black bg-opacity-90"></div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div x-ref="content" class="px-3 py-1 text-xs rounded-full cursor-pointer text-neutral-500 bg-neutral-100">${$parts->first()}</div>
+
+                            </div>
+                        `;
+                    case 2:
+                        return $parts->last();
+                    case 3:
+                        return $parts->last();
+                    default:
+                        return $match[1];
+
+                }
+            });
     }
 }
