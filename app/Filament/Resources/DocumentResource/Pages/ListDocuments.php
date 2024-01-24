542373e26147f4515399a7a20398588fb027a9dc
Jon Fackrell <jon.fackrell@gmail.com>
2024-01-24 10:27:54 -0700

    Changed export to header action rather than bulk action

--- app/Filament/Resources/DocumentResource/Pages/ListDocuments.php
+++ app/Filament/Resources/DocumentResource/Pages/ListDocuments.php
@@ -2,8 +2,10 @@
 
 namespace App\Filament\Resources\DocumentResource\Pages;
 
+use App\Filament\Exports\DocumentExporter;
 use App\Filament\Resources\DocumentResource;
 use Filament\Actions;
+use Filament\Actions\Exports\Models\Export;
 use Filament\Resources\Pages\ListRecords;
 
 class ListDocuments extends ListRecords
@@ -14,6 +16,10 @@ class ListDocuments extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+            Actions\ExportAction::make()
+                ->exporter(DocumentExporter::class)
+                ->fileName(fn (Export $export): string => "document-metadata-{$export->getKey()}")
+                ->chunkSize(500),
         ];
     }
 }
