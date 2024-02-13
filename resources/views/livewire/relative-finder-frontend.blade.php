1410640e4e86e3607e48c4c50b9181689d1d92e4
Jon Fackrell <jon.fackrell@gmail.com>
2024-02-13 11:48:26 -0700

    added banner

--- resources/views/livewire/relative-finder-frontend.blade.php
+++ resources/views/livewire/relative-finder-frontend.blade.php
@@ -1,6 +1,9 @@
 <div x-data="relationshipChecker">
+    <x-banner-image :image="asset('img/banners/people.png')"
+                    :text="'My Relatives'"
+    />
     <div class="">
-        <div class="py-8 mx-auto max-w-7xl">
+        <div class="py-4 mx-auto max-w-7xl">
             @if(session()->has('status'))
                 <div class="py-6 px-8">
                     <div class="relative py-3 px-4 text-green-700 bg-green-100 rounded border border-green-400" role="alert">
