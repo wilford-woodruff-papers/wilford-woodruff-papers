2505f82bd8ecf48552f5a055b9d7f96d5272f7ff
Jon Fackrell <jon.fackrell@gmail.com>
2024-02-15 15:25:23 -0700

    created relative finder page

---
+++ app/Http/Controllers/FindMyRelativesController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class FindMyRelativesController extends Controller
+{
+    public function __invoke()
+    {
+        return view('find-my-relatives');
+    }
+}
