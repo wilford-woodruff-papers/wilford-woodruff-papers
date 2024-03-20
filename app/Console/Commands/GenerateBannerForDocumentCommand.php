45e1dbe6bb6c7facbe80ef0514fcd693d877f9d5
Jon Fackrell <jon.fackrell@gmail.com>
2024-03-20 09:14:38 -0600

    moved screenshot logic to job

--- app/Console/Commands/GenerateBannerForDocumentCommand.php
+++ app/Console/Commands/GenerateBannerForDocumentCommand.php
@@ -2,9 +2,8 @@
 
 namespace App\Console\Commands;
 
-use App\Models\Item;
+use App\Jobs\GenerateDocumentBannerJob;
 use Illuminate\Console\Command;
-use Spatie\Browsershot\Browsershot;
 
 class GenerateBannerForDocumentCommand extends Command
 {
@@ -14,33 +13,8 @@ class GenerateBannerForDocumentCommand extends Command
 
     public function handle(): int
     {
-        $base64Data = Browsershot::url(route('document-banner', [
-            'item' => $this->argument('item'),
-        ]))
-            ->newHeadless()
-            //->setChromePath('/usr/bin/google-chrome')
-            ->setChromePath((app()->environment('local') ? '/opt/homebrew/bin/chromium' : '/usr/bin/google-chrome'))
-            //->setOption('addStyleTag', json_encode([
-            //    'content' => '#tips, #controls, #deck-progress { display: none; }',
-            //]))
-            ->windowSize(1232, 371)
-            //->fit(Manipulations::FIT_CONTAIN, 400, 400)
-            ->setScreenshotType('jpeg', 100)
-            ->waitUntilNetworkIdle()
-            ->base64Screenshot();
-
-        //dd($base64Data);
-        //Storage::disk('day_in_the_life')
-        //        Storage::disk('local')
-        //            ->put($month.'/'.$day.'.jpg', base64_decode($base64Data));
-        // dd($date);
-        $item = Item::whereUuid($this->argument('item'))->first();
-
-        $item->clearMediaCollection('default');
-
-        $item->addMediaFromBase64($base64Data)
-            ->usingFileName(str($item->name)->slug()->toString().'.jpg')
-            ->toMediaCollection('default');
+        GenerateDocumentBannerJob::dispatch($this->argument('item'))
+            ->onQueue('import');
 
         return Command::SUCCESS;
     }
