5dea058e1b432cfe8d17f813812cc2247228be78
Jon Fackrell <jon.fackrell@gmail.com>
2024-04-04 12:59:14 -0600

    added image width

--- resources/views/public/figures/index.blade.php
+++ resources/views/public/figures/index.blade.php
@@ -45,7 +45,7 @@
                                         <td class="py-4 pr-3 pl-4 text-sm font-medium text-gray-900 whitespace-nowrap sm:pl-0 min-w-[8em]">
                                             <img src="{{ Storage::disk('figures')->url($figure->filename) }}"
                                                  alt="{{ $figure->design_description }}"
-                                                 class="h-auto"
+                                                 class="h-auto w-[8em]"
                                             />
                                         </td>
                                         <td class="py-4 px-3 text-lg text-gray-900 whitespace-nowrap">
