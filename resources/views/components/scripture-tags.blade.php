fc24d0dc24154ce41255463f1af709192b8158b7
Jon Fackrell <jon.fackrell@gmail.com>
2024-05-09 15:03:38 -0600

    update scripture search results and FAIR banner

---
+++ resources/views/components/scripture-tags.blade.php
@@ -0,0 +1,16 @@
+<div>
+    @if(! empty($volumes))
+        <div class="flex flex-wrap gap-4 mt-2 serif">
+            @foreach($volumes as $key => $volume)
+                <div class="inline-flex gap-x-2 items-center py-1 px-2 text-white bg-secondary">
+                    <div class="text-sm font-semibold">
+                        {{ $key }}:
+                    </div>
+                    <div class="">
+                        {{ implode(', ', $volume) }}
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    @endif
+</div>
