9bbb2a78ea5bac5f55b3e1a4709ddfacafa29b20
Shift <shift@laravelshift.com>
2024-09-20 14:11:04 +0000

    Re-register HTTP middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,33 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectGuestsTo(fn () => route('login'));
+        $middleware->redirectUsersTo(RouteServiceProvider::HOME);
+
+        $middleware->validateCsrfTokens(except: [
+            'login/constantcontact/callback',
+            'testimonials',
+            'content-page/*/upload',
+            'horizon/*',
+        ]);
+
+        $middleware->throttleWithRedis();
+
+        $middleware->web([
+            \Laravel\Jetstream\Http\Middleware\AuthenticateSession::class,
+            \App\Http\Middleware\Localization::class,
+        ]);
+
+        $middleware->throttleApi();
+
+        $middleware->replace(\Illuminate\Http\Middleware\TrustProxies::class, \App\Http\Middleware\TrustProxies::class);
+
+        $middleware->alias([
+            'ai-download' => \App\Http\Middleware\DownloadAIExperienceMiddleware::class,
+            'api-terms' => \App\Http\Middleware\RequireApiTermsAcceptanceMiddleware::class,
+            'honeypot' => \Spatie\Honeypot\ProtectAgainstSpam::class,
+            'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
