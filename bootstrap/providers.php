a3d163051de4c101915485536160e5a8a347a9f5
Jon Fackrell <jon.fackrell@gmail.com>
2024-10-02 16:56:12 -0600

    Updated familysearch socialite integration for Laravel 11 upgrade

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -7,4 +7,5 @@ return [
     App\Providers\FortifyServiceProvider::class,
     App\Providers\JetstreamServiceProvider::class,
     App\Providers\HorizonServiceProvider::class,
+    SocialiteProviders\Manager\ServiceProvider::class
 ];
