927f77449d776070cc4f4f634198c7d5544554da
Jon Fackrell <jon.fackrell@gmail.com>
2024-12-06 22:20:27 -0700

    add WWP to relative finder labels

--- app/Console/Commands/CacheGender.php
+++ app/Console/Commands/CacheGender.php
@@ -2,8 +2,8 @@
 
 namespace App\Console\Commands;
 
-use Illuminate\Console\Command;
 use App\Models\Subject;
+use Illuminate\Console\Command;
 
 class CacheGender extends Command
 {
@@ -27,36 +27,37 @@ class CacheGender extends Command
     public function handle()
     {
         $people = Subject::query()
-            ->select(["id", "familysearch_person", "gender"])
+            ->select(['id', 'familysearch_person', 'gender'])
             ->people()
-            ->whereNull("gender")
-            ->whereNotNull("familysearch_person")
+            ->whereNull('gender')
+            ->whereNotNull('familysearch_person')
             ->limit(50)
             ->get();
 
         foreach ($people as $person) {
             $gender = data_get(
                 json_decode($person->familysearch_person),
-                "persons.0.gender.type"
+                'persons.0.gender.type'
             );
             // print_r($gender . "\n");
             if (empty($gender)) {
                 logger()->info("No Gender Available\n");
+
                 // print_r($person->familysearch_person . "\n");
                 continue;
             }
             $calculatedGender = null;
-            if (str($gender)->endsWith("/Female")) {
-                $calculatedGender = "F";
-            } elseif (str($gender)->endsWith("/Male")) {
-                $calculatedGender = "M";
+            if (str($gender)->endsWith('/Female')) {
+                $calculatedGender = 'F';
+            } elseif (str($gender)->endsWith('/Male')) {
+                $calculatedGender = 'M';
             }
 
-            logger()->info($calculatedGender . "\n");
+            logger()->info($calculatedGender."\n");
 
-            if (!empty($calculatedGender)) {
+            if (! empty($calculatedGender)) {
                 $person->update([
-                    "gender" => $calculatedGender
+                    'gender' => $calculatedGender,
                 ]);
             }
         }
